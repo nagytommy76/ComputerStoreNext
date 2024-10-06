168135e4a779d21c28773cb63385ee6fc3ea313f
nagytommy76 <nagytommy76@gmail.com>
2024-10-06 12:26:59 +0200

    Created a dropdown menu for shop links

---
+++ components/Navbar/Menu/NavMenu.tsx
@@ -0,0 +1,43 @@
+'use client'
+import { useState } from 'react'
+import Link from 'next/link'
+
+import Button from '@mui/material/Button'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+
+const NavMenu = () => {
+   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+   const open = Boolean(anchorEl)
+   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+      setAnchorEl(event.currentTarget)
+   }
+   const handleClose = () => {
+      setAnchorEl(null)
+   }
+
+   return (
+      <>
+         <Button size='large' onClick={handleClick}>
+            Shop menü
+         </Button>
+         <Menu
+            id='basic-menu'
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+               'aria-labelledby': 'basic-button',
+            }}
+         >
+            <MenuItem onClick={handleClose}>
+               <Link href={'/shop/vga'}>Videókártya</Link>
+            </MenuItem>
+            <MenuItem onClick={handleClose}>Alaplap</MenuItem>
+            <MenuItem onClick={handleClose}>Processzor</MenuItem>
+         </Menu>
+      </>
+   )
+}
+
+export default NavMenu
