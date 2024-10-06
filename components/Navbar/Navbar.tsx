ffd9a43a04c15edb987a5c32b14e0925510793c1
nagytommy76 <nagytommy76@gmail.com>
2024-10-06 12:27:52 +0200

    Using NavMenu & modified the toolbar styles

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,5 +1,3 @@
-'use client'
-// import { useState } from 'react'
 import Link from 'next/link'
 
 import AppBar from '@mui/material/AppBar'
@@ -7,34 +5,30 @@ import Toolbar from '@mui/material/Toolbar'
 
 import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
-import Stack from '@mui/material/Stack'
-
-import Button from '@mui/material/Button'
-
-// import Menu from '@mui/material/Menu'
-// import MenuItem from '@mui/material/MenuItem'
-
 import Typography from '@mui/material/Typography'
 
 import ThemeToggle from './ThemeToggler/Toggle'
+import NavMenu from './Menu/NavMenu'
 
 const Navbar = () => {
-   // const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null)
-
    return (
       <AppBar component={'nav'} position='sticky'>
          <Container maxWidth='xl'>
-            <Toolbar disableGutters sx={{ width: '100%', display: 'flex', justifyContent: 'space-between' }}>
-               <Typography variant='h4'>ComputerStore</Typography>
-               <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                  <Stack direction={'row'} spacing={1}>
-                     <Link href='/'>
-                        <Button sx={{ my: 2, color: 'white', display: 'block' }}>Föoldal</Button>
-                     </Link>
-                     <Link href='/'>
-                        <Button sx={{ my: 2, color: 'white', display: 'block' }}>Shop menü</Button>
-                     </Link>
-                  </Stack>
+            <Toolbar
+               disableGutters
+               sx={{
+                  width: '100%',
+                  height: '80px',
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+               }}
+            >
+               <Link href='/'>
+                  <Typography variant='h4'>ComputerStore</Typography>
+               </Link>
+               <Box sx={{ display: { xs: 'none', md: 'flex' }, alignItems: 'center' }}>
+                  <NavMenu />
                   <ThemeToggle />
                </Box>
             </Toolbar>
