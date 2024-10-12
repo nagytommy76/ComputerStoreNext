d1f50d8613066b94d771961d1e0862dd8831d5ab
nagytommy76 <nagytommy76@gmail.com>
2024-10-12 15:02:47 +0200

    Created a style file

---
+++ app/shop/vga/styles/style.ts
@@ -0,0 +1,10 @@
+export const CardGridContainerStyle = {
+   minHeight: '100vh',
+   display: 'grid',
+   width: '85%',
+   rowGap: '3rem',
+   columnGap: '2rem',
+   justifyContent: 'center',
+   gridAutoRows: '380px',
+   gridTemplateColumns: 'repeat(auto-fill, minmax(225px, 1fr))',
+}
