dede733338e3751a6e335bc90f9bb73670e01256
nagytommy76 <nagytommy76@gmail.com>
2024-10-13 13:26:06 +0200

    Added image paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+   images: {
+      remotePatterns: [
+         {
+            protocol: 'https',
+            hostname: 'media.icdn.hu',
+         },
+         {
+            protocol: 'https',
+            hostname: 'www.techpowerup.com',
+         },
+         {
+            protocol: 'https',
+            hostname: 'media.cdn.sapphiretech.com.cn',
+         },
+         {
+            protocol: 'https',
+            hostname: 'cdn.alza.hu',
+         },
+         {
+            protocol: 'https',
+            hostname: 'asset.msi.com',
+         },
+         {
+            protocol: 'https',
+            hostname: 'static.gigabyte.com',
+         },
+      ],
+   },
+}
 
-export default nextConfig;
+export default nextConfig
