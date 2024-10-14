7ff995b79e87c5b8f50637649a7204c243714fba
nagytommy76 <nagytommy76@gmail.com>
2024-10-14 18:57:11 +0200

    Created BaseFetchedProductType

---
+++ types/productType.ts
@@ -0,0 +1,10 @@
+export interface BaseFetchedProductType {
+   _id: string
+   itemNumber: string
+   type: string
+   typeCode: string
+   manufacturer: string
+   price: number
+   pictureUrls: string[]
+   ratingValues: string[]
+}
