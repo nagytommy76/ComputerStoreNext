9f911af5bba673f3cd5d9a8591a1334a8afb0c1c
nagytommy76 <nagytommy76@gmail.com>
2024-10-16 19:50:49 +0200

    Imported dbConnect

--- app/actions/getVgaActions.tsx
+++ app/actions/getVgaActions.tsx
@@ -1,6 +1,7 @@
 'use server'
 import { VgaProduct } from '@/models/vga'
-import type { BaseProductType } from '@Types/baseModelTypes'
+import type { BaseFetchedProductType } from '@/types/productType'
+import dbConnect from '@DBConnect'
 
 interface BaseFilterType {
    currentPage: number
@@ -44,13 +45,14 @@ const filterVgaData: VgaFilterType = {
 }
 
 export default async function getVga(filterVga: VgaFilterType = filterVgaData) {
+   await dbConnect()
    const {
       //   currentPage,
       //   perPage,
       orderBy,
       byManufacturer,
       priceRange,
-      selectedWarranty,
+      // selectedWarranty,
       productName,
       baseClock,
       boostClock,
@@ -64,27 +66,28 @@ export default async function getVga(filterVga: VgaFilterType = filterVgaData) {
    } = filterVga
 
    try {
-      const foundVgas = await VgaProduct.find<BaseProductType>({
-         //  manufacturer: new RegExp(byManufacturer, 'i'),
-         //  $or: [
-         //     { type: new RegExp(productName, 'i') },
-         //     { typeCode: new RegExp(productName, 'i') },
-         //     { itemNumber: new RegExp(productName, 'i') },
-         //  ],
-         //  warranty: selectedWarranty === 'all' ? null : { 'details.warranity': selectedWarranty },
-         //  price: { $gte: priceRange[0], $lte: priceRange[1] },
-         //  'details.gpuBaseClock': { $gte: baseClock[0], $lte: baseClock[1] },
-         //  'details.gpuPeakClock': { $gte: boostClock[0], $lte: boostClock[1] },
-         //  'details.length': { $gte: length[0], $lte: length[1] },
-         //  'details.powerConsuption': { $gte: tdp[0], $lte: tdp[1] },
-         //  'details.vramBandwidth': { $gte: vramBandwidth[0], $lte: vramBandwidth[1] },
-         //  'details.vramCapacity': { $gte: vramCapacity[0], $lte: vramCapacity[1] },
-         //  'details.gpuManufacturer': new RegExp(gpuManufacturer, 'i'),
-         //  'details.pcieType': new RegExp(pciType, 'i'),
-         //  'details.vramType': new RegExp(vramType, 'i'),
-      }).select('itemNumber price manufacturer type typeCode pictureUrls ratingValues._id')
-      //  .sort({ price: orderBy })
-      console.log(foundVgas)
+      const foundVgas = await VgaProduct.find<BaseFetchedProductType>({
+         manufacturer: new RegExp(byManufacturer, 'i'),
+         $or: [
+            { type: new RegExp(productName, 'i') },
+            { typeCode: new RegExp(productName, 'i') },
+            { itemNumber: new RegExp(productName, 'i') },
+         ],
+         price: { $gte: priceRange[0], $lte: priceRange[1] },
+         // 'details.warranity': new RegExp(selectedWarranty, 'i'),
+         'details.gpuBaseClock': { $gte: baseClock[0], $lte: baseClock[1] },
+         'details.gpuPeakClock': { $gte: boostClock[0], $lte: boostClock[1] },
+         'details.length': { $gte: length[0], $lte: length[1] },
+         'details.powerConsuption': { $gte: tdp[0], $lte: tdp[1] },
+         'details.vramBandwidth': { $gte: vramBandwidth[0], $lte: vramBandwidth[1] },
+         'details.vramCapacity': { $gte: vramCapacity[0], $lte: vramCapacity[1] },
+         'details.gpuManufacturer': new RegExp(gpuManufacturer, 'i'),
+         'details.pcieType': new RegExp(pciType, 'i'),
+         'details.vramType': new RegExp(vramType, 'i'),
+      })
+         .select('itemNumber price manufacturer type typeCode pictureUrls ratingValues._id')
+         .sort({ price: orderBy })
+
       return foundVgas
    } catch (error) {
       console.log(error)
