61800b98bd787d04957230106915c7e22873771c
nagytommy76 <nagytommy76@gmail.com>
2024-10-27 14:37:25 +0100

    Added FilterTypes

---
+++ types/filterTypes.ts
@@ -0,0 +1,11 @@
+export type FilterTypes = {
+   orderBy: string
+   minPrice: number
+   maxPrice: number
+   priceRange: number[]
+   allManufacturer: string[]
+   selectedManufacturer: string
+   productName: string
+   allWarranties: string[]
+   selectedWarranty: string
+}
