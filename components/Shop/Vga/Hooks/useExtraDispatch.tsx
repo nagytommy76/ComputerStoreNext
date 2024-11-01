13418e1f6a3cdb04d89f8ea1ac371615fa6c0246
nagytommy76 <nagytommy76@gmail.com>
2024-11-01 14:31:47 +0100

    Dispatching the vga filter data

---
+++ components/Shop/Vga/Hooks/useExtraDispatch.tsx
@@ -0,0 +1,71 @@
+import { useAppDispatch } from '@/reduxStore/hooks'
+import {
+   setBaseClockRange,
+   setBoostClockRange,
+   setGpuManufacturers,
+   setLengths,
+   setPcieTypes,
+   setPowerConsuptions,
+   setSelectedBaseClockRange,
+   setSelectedBoostClockRange,
+   setSelectedLength,
+   setSelectedPowerConsuption,
+   setSelectedVramBandwidth,
+   setSelectedVramCapRange,
+   setVramBandwidths,
+   setVramCapacitiyRange,
+   setVramTypes,
+} from '@/reduxStore/slices/Filter/VgaFilterSlice'
+
+export default function useExtraDispatch() {
+   const dispatch = useAppDispatch()
+
+   const extraDispatches = (params: IncomingParamsType) => {
+      dispatch(setGpuManufacturers(params.gpuManufacturer))
+      dispatch(setPcieTypes(params.pciType))
+      dispatch(setVramTypes(params.vramType))
+
+      const baseClockRange = [params.minBaseClock, params.maxBaseClock]
+      dispatch(setBaseClockRange(baseClockRange))
+      dispatch(setSelectedBaseClockRange(baseClockRange))
+
+      const boostClockRange = [params.minBoostClock, params.maxBoostClock]
+      dispatch(setBoostClockRange(boostClockRange))
+      dispatch(setSelectedBoostClockRange(boostClockRange))
+
+      const lengthRange = [params.minLength, params.maxLength]
+      dispatch(setLengths(lengthRange))
+      dispatch(setSelectedLength(lengthRange))
+
+      const TDPRange = [params.minTdp, params.maxTdp]
+      dispatch(setPowerConsuptions(TDPRange))
+      dispatch(setSelectedPowerConsuption(TDPRange))
+
+      const vramBandwidthRange = [params.minVramBandwidth, params.maxVramBandwidth]
+      dispatch(setVramBandwidths(vramBandwidthRange))
+      dispatch(setSelectedVramBandwidth(vramBandwidthRange))
+
+      const vramCapacityRange = [params.minVramCapacity, params.maxVramCapacity]
+      dispatch(setVramCapacitiyRange(vramCapacityRange))
+      dispatch(setSelectedVramCapRange(vramCapacityRange))
+   }
+
+   return extraDispatches
+}
+type IncomingParamsType = {
+   minBaseClock: number
+   maxBaseClock: number
+   minBoostClock: number
+   maxBoostClock: number
+   gpuManufacturer: string[]
+   minLength: number
+   maxLength: number
+   pciType: string[]
+   minTdp: number
+   maxTdp: number
+   minVramBandwidth: number
+   maxVramBandwidth: number
+   minVramCapacity: number
+   maxVramCapacity: number
+   vramType: string[]
+}
