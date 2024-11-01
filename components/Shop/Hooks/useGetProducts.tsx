f004db0038230c9fcb76ff02aace34ac783e0e21
nagytommy76 <nagytommy76@gmail.com>
2024-11-01 14:32:37 +0100

    Using useQuery on success function to dispatch the products

--- components/Shop/Hooks/useGetProducts.tsx
+++ components/Shop/Hooks/useGetProducts.tsx
@@ -7,6 +7,7 @@ import type { BaseFetchedProductType } from '@/types/productType'
 export default function useGetProducts(productType: string = 'vga', extraQueryParameters: string = '') {
    const dispatch = useAppDispatch()
    const filterOptions = useAppSelector((state) => state.filter)
+
    const queryFunction = async () => {
       const response = await fetch(
          `/api/${productType}/get-all-vga?orderBy=${filterOptions.orderBy}&byManufacturer=${
@@ -19,14 +20,13 @@ export default function useGetProducts(productType: string = 'vga', extraQueryPa
       return (await response.json()) as Promise<{ products: BaseFetchedProductType[] }>
    }
 
-   const { data, error, isLoading } = useQuery({
-      queryKey: ['getAllVga'],
+   useQuery({
+      queryKey: [`get-all-${productType}`, filterOptions, productType],
       queryFn: queryFunction,
+      onSuccess: (data) => {
+         dispatch(setProducts(data.products))
+      },
    })
 
-   if (!error && data) {
-      dispatch(setProducts(data.products))
-   }
-
-   return { products: data?.products || [], error, isLoading }
+   return null
 }
