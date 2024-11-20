8bdcf8d8c3ce0e9e5feec45787d8c716da1c3bfa
nagytommy76 <nagytommy76@gmail.com>
2024-11-20 19:07:58 +0100

    Using the vga filter components

--- components/Shop/Vga/VgaShopPage.tsx
+++ components/Shop/Vga/VgaShopPage.tsx
@@ -5,20 +5,39 @@ import useFilter from '../Hooks/UseFilter'
 
 import useExtraDispatch from './Hooks/useExtraDispatch'
 import useExtraQuery from './Hooks/useExtraQuery'
+import type { FilterTypes } from '@/types/filterTypes'
 
 import BaseSideFilter from '../SideFilter/BaseSideFilter'
 import Products from '../Products/Products'
 
-export default function VgaShopPage() {
+import ByBandwidth from './Filters/ByBandwidth'
+import ByBaseClock from './Filters/ByBaseClock'
+import ByBoostClock from './Filters/ByBoostClock'
+import ByCapacity from './Filters/ByCapacity'
+import ByGpuManufacturer from './Filters/ByGpuManufacturer'
+import ByLength from './Filters/ByLength'
+import ByPciType from './Filters/ByPciType'
+import ByPowerConsumption from './Filters/ByPowerConsumption'
+import ByVramType from './Filters/ByVramType'
+
+export default function VgaShopPage({ vgaFilterData }: { vgaFilterData: FilterTypes }) {
    const extraDispatches = useExtraDispatch()
    const extraQuery = useExtraQuery()
-   useGetProducts('vga', extraQuery)
-   useFilter('vga', extraDispatches)
+   const isFetched = useFilter(vgaFilterData, extraDispatches)
+   useGetProducts('vga', extraQuery, isFetched)
 
    return (
       <Products>
          <BaseSideFilter>
-            <>Extra kereső</>
+            <ByGpuManufacturer />
+            <ByPciType />
+            <ByVramType />
+            <ByBandwidth />
+            <ByBaseClock />
+            <ByBoostClock />
+            <ByCapacity />
+            <ByLength />
+            <ByPowerConsumption />
          </BaseSideFilter>
       </Products>
    )
