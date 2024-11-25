f8d7d8a44f9a52c5ae852bbef3c3d726bace0970
nagytommy76 <nagytommy76@gmail.com>
2024-11-25 18:54:06 +0100

    Using totalProducts & search field component

---
+++ components/Shop/Products/Header/Header.tsx
@@ -0,0 +1,27 @@
+import Typography from '@mui/material/Typography'
+import { useAppSelector } from '@/utils/redux/hooks'
+
+import SearchField from './SearchField'
+
+export default function Header({ productName }: { productName: string }) {
+   const totalProducts = useAppSelector((state) => state.filter.totalProducts)
+   return (
+      <div
+         style={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            gap: '1rem',
+            marginBottom: '1.5rem',
+         }}
+      >
+         <Typography variant='h3' gutterBottom>
+            {productName}
+         </Typography>
+         <Typography variant='body2' color='text.secondary' gutterBottom>
+            ({totalProducts} termék)
+         </Typography>
+         <SearchField />
+      </div>
+   )
+}
