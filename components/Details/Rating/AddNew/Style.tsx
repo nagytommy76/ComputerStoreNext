38ef3bf3282069ebca176e38416c6043df274971
nagytommy76 <nagytommy76@gmail.com>
2024-12-03 18:58:28 +0100

    Added a RatingContainer style

---
+++ components/Details/Rating/AddNew/Style.tsx
@@ -0,0 +1,8 @@
+'use client'
+import { styled } from '@mui/material/styles'
+
+export const RatingContainer = styled('div')({
+   display: 'flex',
+   flexDirection: 'column',
+   margin: '0 auto',
+})
