c8700c42b50765a7fc14fcb45e69a89f2791b954
nagytommy76 <nagytommy76@gmail.com>
2024-12-08 12:44:14 +0100

    Added StyledCard

--- components/Details/Rating/AddNew/Style.tsx
+++ components/Details/Rating/AddNew/Style.tsx
@@ -1,8 +1,27 @@
 'use client'
 import { styled } from '@mui/material/styles'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
 
 export const RatingContainer = styled('div')({
    display: 'flex',
    flexDirection: 'column',
-   margin: '0 auto',
+   margin: '2rem auto',
 })
+
+export const StyledCard = styled(Card)(({ theme }) => ({
+   width: '100%',
+   minHeight: 350,
+
+   display: 'flex',
+   flexDirection: 'row',
+   justifyContent: 'center',
+
+   [`@media (max-width: ${theme.breakpoints.values.sm}px)`]: {
+      flexDirection: 'column',
+      alignItems: 'center',
+   },
+}))
+export const StyledCardContent = styled(CardContent)(({}) => ({
+   flex: 1,
+}))
