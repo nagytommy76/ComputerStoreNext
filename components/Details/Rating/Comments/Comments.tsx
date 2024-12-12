3f91470d537e11057851e6797634c649af97bd2e
nagytommy76 <nagytommy76@gmail.com>
2024-12-12 11:26:01 +0100

    Display the comments

--- components/Details/Rating/Comments/Comments.tsx
+++ components/Details/Rating/Comments/Comments.tsx
@@ -2,16 +2,22 @@ import { useContext } from 'react'
 import { RatingContext } from '../Context/RatingContext'
 import { CommentAnswerProvider } from './Context/AnswerContext'
 
+import SingleComment from './SingleComment/SingleComment'
+
 export default function Comments() {
    const { comments } = useContext(RatingContext)
+   if (comments?.length === 0) return null
    return (
       <>
-         {comments.map((comment) => {
-            ;<CommentAnswerProvider commentId={comment._id} commentAnswersProp={comment.commentAnswers}>
-               <h1>Kommentek</h1>
-               <h2>sdsda</h2>
+         {comments.map((comment) => (
+            <CommentAnswerProvider
+               key={comment._id}
+               commentId={comment._id}
+               commentAnswersProp={comment.commentAnswers}
+            >
+               <SingleComment comment={comment} />
             </CommentAnswerProvider>
-         })}
+         ))}
       </>
    )
 }
