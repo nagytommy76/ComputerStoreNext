88c68b9cc504774bd4d1a05ec71470c57d5a405a
nagytommy76 <nagytommy76@gmail.com>
2024-12-14 14:06:06 +0100

    Removed the styles -> moved to app/login

--- components/Auth/Login/Login.tsx
+++ components/Auth/Login/Login.tsx
@@ -1,31 +1,29 @@
 import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
-import LoginImage from '@images/login.jpg'
 
-import { LoginPage, LoginContainer, LoginForm, StyledImage } from './Styles'
+import loginAction from '@/serverActions/Login/loginAction'
+
+import { LoginForm } from './Styles'
 
 export default function Login() {
    return (
-      <LoginPage>
-         <LoginContainer>
-            <LoginForm component='form' action={''} method='post'>
-               <Typography variant='h4' align='center'>
-                  Bejelentkezés
-               </Typography>
-               <TextField id='email' name='email' label='Email cím' variant='outlined' fullWidth />
-               <TextField id='password' name='password' label='Jelszó' variant='outlined' fullWidth />
-               <Button type='submit' variant='outlined'>
-                  Belépés
-               </Button>
-            </LoginForm>
-         </LoginContainer>
-         <StyledImage
-            src={LoginImage.src}
-            alt='login page image with a cpu cooler'
-            width={1000}
-            height={1200}
+      <LoginForm component='form' action={loginAction} method='post'>
+         <Typography variant='h4' align='center'>
+            Bejelentkezés
+         </Typography>
+         <TextField type='email' id='email' name='email' label='Email cím' variant='outlined' fullWidth />
+         <TextField
+            type='password'
+            id='password'
+            name='password'
+            label='Jelszó'
+            variant='outlined'
+            fullWidth
          />
-      </LoginPage>
+         <Button type='submit' variant='outlined'>
+            Belépés
+         </Button>
+      </LoginForm>
    )
 }
--- components/Auth/Login/Styles.tsx
+++ components/Auth/Login/Styles.tsx
@@ -1,26 +1,6 @@
 'use client'
 import { styled } from '@mui/material/styles'
-import Image from 'next/image'
 import Paper from '@mui/material/Paper'
-
-export const LoginPage = styled('section')(({ theme }) => ({
-   minHeight: '80vh',
-   display: 'grid',
-   gridTemplateColumns: 'repeat(2, 50%)',
-   alignItems: 'center',
-   justifyContent: 'center',
-
-   [`@media (max-width: ${theme.breakpoints.values.sm}px)`]: {
-      gridTemplateColumns: 'repeat(1, 95%)',
-   },
-}))
-
-export const LoginContainer = styled('div')({
-   display: 'flex',
-   flexDirection: 'row',
-   alignItems: 'center',
-   justifyContent: 'center',
-})
 
 export const LoginForm = styled(Paper)({
    width: '450px',
@@ -32,11 +12,3 @@ export const LoginForm = styled(Paper)({
    flexDirection: 'column',
    justifyContent: 'space-around',
 })
-
-export const StyledImage = styled(Image)(({ theme }) => ({
-   width: '100%',
-   objectFit: 'cover',
-   [`@media (max-width: ${theme.breakpoints.values.sm}px)`]: {
-      display: 'none',
-   },
-}))
