64498ac769cd7b02842ff8d63448336dd53c7ad9
nagytommy76 <nagytommy76@gmail.com>
2024-12-15 12:53:54 +0100

    Created the User model

---
+++ models/User/User.ts
@@ -0,0 +1,66 @@
+import type { UserTypes } from '@/types/userTypes'
+import { Schema, model, Model, models } from 'mongoose'
+
+const UserSchema = new Schema<UserTypes, Model<UserTypes>>({
+   userName: { type: String, required: true, unique: true },
+   password: {
+      type: String,
+      required: [true, 'Adjon meg egy jelszót!'],
+      minlength: [6, 'a jelszó min. 6 karakter legyen!'],
+   },
+   email: {
+      type: String,
+      required: [true, 'Adjon meg egy email címet!'],
+      unique: true,
+      lowercase: true,
+   },
+   isAdmin: { type: Boolean, default: false },
+   isEmailConfirmed: { type: Boolean, default: false },
+   orders: [
+      {
+         orderedAt: { type: Date, default: new Date() },
+         totalPrice: { type: Number, default: 0 },
+         deliveryType: { type: String, default: '' },
+         deliveryPrice: { type: Number, default: 0 },
+         paymentMethod: { type: String, default: '' },
+         payedAt: { type: Number, default: 0 },
+         products: {
+            type: [
+               {
+                  productID: Schema.Types.ObjectId,
+                  productName: String,
+                  productImage: String,
+                  productQty: Number,
+                  productPrice: Number,
+                  productType: String,
+               },
+            ],
+         },
+      },
+   ],
+   userDetails: {
+      firstName: String,
+      lastName: String,
+      phone: String,
+      address: {
+         zipCode: Number,
+         city: String,
+         street: String,
+         houseNumber: String,
+         floor: String,
+         door: String,
+      },
+   },
+   cartItems: [
+      {
+         itemId: String,
+         displayImage: String,
+         displayName: String,
+         price: Number,
+         quantity: Number,
+         productType: String,
+      },
+   ],
+})
+
+export const UserModel = models.UserModel || model<UserTypes>('UserModel', UserSchema)
