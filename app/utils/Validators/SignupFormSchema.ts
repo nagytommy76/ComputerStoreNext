099b2eb21d294cccd3226033fe4bb31da3255c71
nagytommy76 <nagytommy76@gmail.com>
2024-12-15 12:57:46 +0100

    Using zod

--- app/utils/Validators/SignupFormSchema.ts
+++ app/utils/Validators/SignupFormSchema.ts
@@ -2,26 +2,15 @@ import { z } from 'zod'
 
 export default function SignupFormSchema() {
    return z.object({
-      email: z.string().email({ message: 'Please enter a valid email.' }).trim(),
+      email: z.string().email({ message: 'Kérlek adj meg egy valid e-mail címet' }).trim(),
       password: z
          .string()
-         .min(6, { message: 'Be at least 6 characters long' })
-         .regex(/[a-zA-Z]/, { message: 'Contain at least one letter.' })
-         .regex(/[0-9]/, { message: 'Contain at least one number.' })
-         .regex(/[^a-zA-Z0-9]/, {
-            message: 'Contain at least one special character.',
-         })
+         .min(6, { message: 'Legalább 6 karakter hosszúságú legyen! ' })
+         // .regex(/[a-zA-Z]/, { message: 'Contain at least one letter.' })
+         // .regex(/[0-9]/, { message: 'Contain at least one number.' })
+         // .regex(/[^a-zA-Z0-9]/, {
+         //    message: 'Contain at least one special character.',
+         // })
          .trim(),
    })
 }
-
-export type FormState =
-   | {
-        errors?: {
-           name?: string[]
-           email?: string[]
-           password?: string[]
-        }
-        message?: string
-     }
-   | undefined
