8917d0e2653409dc97699756fade3a9fd7793fdc
nagytommy76 <nagytommy76@gmail.com>
2024-12-17 14:16:26 +0100

    Created createSession fn to store session token -> cookie

--- app/utils/Session.ts
+++ app/utils/Session.ts
@@ -1,10 +1,11 @@
 import 'server-only'
 import { SignJWT, jwtVerify } from 'jose'
+import { cookies } from 'next/headers'
 
 const secretKey = process.env.SESSION_SECRET
 const encodedKey = new TextEncoder().encode(secretKey)
 
-export async function encrypt(payload: JWTPayload, expirationTime: string = '7d') {
+export async function encrypt(payload: JWTPayload, expirationTime: string | number | Date = '7d') {
    return new SignJWT(payload)
       .setProtectedHeader({ alg: 'HS256' })
       .setIssuedAt()
@@ -22,6 +23,20 @@ export async function decrypt(session: string | undefined = '') {
    } catch (error) {
       console.log('Failed to verify session')
    }
+}
+
+export async function createSession(payload: JWTPayload) {
+   const expiresAt = new Date(Date.now() + 2 * 24 * 60 * 60 * 1000) // 2 nap * 24 óra * 1óra * 1 perc
+   const sessionToken = await encrypt(payload, expiresAt)
+   const cookieStore = await cookies()
+
+   cookieStore.set('sessionToken', sessionToken, {
+      httpOnly: true,
+      secure: true,
+      expires: expiresAt,
+      sameSite: 'lax',
+      path: '/',
+   })
 }
 
 type JWTPayload = {
