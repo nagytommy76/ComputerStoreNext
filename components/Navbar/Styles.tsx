4f82b5f3cc2a81d0446dbb2a33d19d8ca2b36abf
nagytommy76 <nagytommy76@gmail.com>
2024-12-19 12:15:55 +0100

    Created a style file for navbar

---
+++ components/Navbar/Styles.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { styled } from '@mui/material/styles'
+import Toolbar from '@mui/material/Toolbar'
+
+export const StyledToolbar = styled(Toolbar)({
+   width: '100%',
+   height: '80px',
+   display: 'flex',
+   justifyContent: 'space-between',
+   alignItems: 'center',
+})
