a2bc045d2333fb4f764a812c5a4b420fe43c9963
nagytommy76 <nagytommy76@gmail.com>
2024-12-22 13:31:25 +0100

    Export nextauth route handlers

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,2 @@
-import NextAuth from 'next-auth'
-
-import options from './options'
-
-const handler = NextAuth(options)
-
-export { handler as GET, handler as POST }
+import { handlers } from '@NextAuth'
+export const { GET, POST } = handlers
