5fde8d033d5fa84511b50f2ccee1887b9c12fb45
nagytommy76 <nagytommy76@gmail.com>
2024-12-23 14:21:33 +0100

    Using useActionState

--- components/Auth/Login/Login.tsx
+++ components/Auth/Login/Login.tsx
@@ -1,24 +1,30 @@
+'use client'
+import { useActionState } from 'react'
 import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 
-// import loginAction from '@/serverActions/Login/loginAction'
-import { signIn } from '@NextAuth'
+import loginAction from '@/serverActions/Login/loginAction'
 
 import { LoginForm } from './Styles'
 
 export default function Login() {
+   const [state, formAction, isPending] = useActionState(loginAction, undefined)
    return (
-      <LoginForm
-         action={async (formData) => {
-            'use server'
-            await signIn('credentials', formData)
-         }}
-      >
+      <LoginForm action={formAction}>
          <Typography variant='h4' align='center'>
             Bejelentkezés
          </Typography>
-         <TextField type='email' id='email' name='email' label='Email cím' variant='outlined' fullWidth />
+         <TextField
+            type='email'
+            id='email'
+            name='email'
+            label='Email cím'
+            variant='outlined'
+            fullWidth
+            error={state?.errors?.email ? true : false}
+            helperText={state?.errors?.email}
+         />
          <TextField
             type='password'
             id='password'
@@ -26,8 +32,10 @@ export default function Login() {
             label='Jelszó'
             variant='outlined'
             fullWidth
+            error={state?.errors?.password ? true : false}
+            helperText={state?.errors?.password}
          />
-         <Button type='submit' variant='outlined'>
+         <Button disabled={isPending} type='submit' variant='outlined'>
             Belépés
          </Button>
       </LoginForm>
