47ed02a47e77c00873fa5dc57d7519ea9d2f4363
nagytommy76 <nagytommy76@gmail.com>
2024-12-23 14:20:54 +0100

    Working on the nextAuth sign in

--- app/actions/Login/loginAction.ts
+++ app/actions/Login/loginAction.ts
@@ -1,11 +1,13 @@
 'use server'
 import { compare } from 'bcrypt'
-import { redirect } from 'next/navigation'
+// import { redirect } from 'next/navigation'
 
 import dbConnect from '@DBConnect'
 import UserModel from '@Models/User/User'
 import SignupFormSchema from '@/Validators/SignupFormSchema'
-import { createSession } from '@/app/utils/Session'
+// import { createSession } from '@/app/utils/Session'
+import { signIn } from '@NextAuth'
+import { AuthError } from 'next-auth'
 
 export default async function loginAction(state: unknown, formData: FormData) {
    const email = formData.get('email')
@@ -35,19 +37,57 @@ export default async function loginAction(state: unknown, formData: FormData) {
    }
    // Check user password
    if (await compare(password as string, user?.password as string)) {
-      await createSession({
+      return {
+         errors: {
+            email: '',
+            password: 'Hiba! Rossz a jelszóó',
+         },
+      }
+   }
+   // await createSession({
+   //    email: user.email,
+   //    userId: user._id,
+   //    userName: user.userName,
+   //    isAdmin: user.isAdmin,
+   // })
+
+   try {
+      await signIn('credentials', {
          email: user.email,
          userId: user._id,
          userName: user.userName,
          isAdmin: user.isAdmin,
+         redirectTo: '/',
       })
-      redirect('/')
-   } else {
-      return {
-         errors: {
-            email: '',
-            password: 'Hiba! Rossz a jelszó',
-         },
+   } catch (error) {
+      if (error instanceof AuthError) {
+         switch (error.type) {
+            case 'CredentialsSignin':
+               return {
+                  errors: {
+                     email: '',
+                     password: 'Hiba! Rossz a jelszó',
+                  },
+               }
+            default: {
+               return {
+                  errors: {
+                     email: '',
+                     password: 'Valami más hiba',
+                  },
+               }
+            }
+         }
       }
    }
+
+   // redirect('/')
+   // } else {
+   //    return {
+   //       errors: {
+   //          email: '',
+   //          password: 'Hiba! Rossz a jelszó',
+   //       },
+   //    }
+   // }
 }
