ed1af534f8522b7907341a8ff03c56a592c5d32e
nagytommy76 <nagytommy76@gmail.com>
2024-12-27 13:44:12 +0100

    Removed the db user checks

--- app/actions/Login/loginAction.ts
+++ app/actions/Login/loginAction.ts
@@ -1,21 +1,13 @@
 'use server'
-import { compare } from 'bcrypt'
-// import { redirect } from 'next/navigation'
-
-import dbConnect from '@DBConnect'
-import UserModel from '@Models/User/User'
 import SignupFormSchema from '@/Validators/SignupFormSchema'
-// import { createSession } from '@/app/utils/Session'
+
 import { signIn } from '@NextAuth'
 import { AuthError } from 'next-auth'
 
 export default async function loginAction(state: unknown, formData: FormData) {
-   const email = formData.get('email')
-   const password = formData.get('password')
-
    const validatedFields = SignupFormSchema().safeParse({
-      email,
-      password,
+      email: formData.get('email'),
+      password: formData.get('password'),
    })
    // If any form fields are invalid, return early
    if (!validatedFields.success) {
@@ -23,40 +15,13 @@ export default async function loginAction(state: unknown, formData: FormData) {
          errors: validatedFields.error.flatten().fieldErrors,
       }
    }
-   await dbConnect()
-   // Search for the user in the database
-   const user = await UserModel.findOne({ email })
 
-   if (!user) {
-      return {
-         errors: {
-            email: 'Nincs ilyen felhasználó',
-            password: '',
-         },
-      }
-   }
-   // Check user password
-   if (await compare(password as string, user?.password as string)) {
-      return {
-         errors: {
-            email: '',
-            password: 'Hiba! Rossz a jelszóó',
-         },
-      }
-   }
-   // await createSession({
-   //    email: user.email,
-   //    userId: user._id,
-   //    userName: user.userName,
-   //    isAdmin: user.isAdmin,
-   // })
+   const { email, password } = validatedFields.data
 
    try {
       await signIn('credentials', {
-         email: user.email,
-         userId: user._id,
-         userName: user.userName,
-         isAdmin: user.isAdmin,
+         email,
+         password,
          redirectTo: '/',
       })
    } catch (error) {
@@ -66,28 +31,18 @@ export default async function loginAction(state: unknown, formData: FormData) {
                return {
                   errors: {
                      email: '',
-                     password: 'Hiba! Rossz a jelszó',
+                     password: 'Hibás belépési adatokat adtál meg!',
                   },
                }
             default: {
                return {
                   errors: {
                      email: '',
-                     password: 'Valami más hiba',
+                     password: `Egyéb hiba: ${error.message}`,
                   },
                }
             }
          }
       }
    }
-
-   // redirect('/')
-   // } else {
-   //    return {
-   //       errors: {
-   //          email: '',
-   //          password: 'Hiba! Rossz a jelszó',
-   //       },
-   //    }
-   // }
 }
