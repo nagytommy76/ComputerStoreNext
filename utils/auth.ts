896ab4329b0fdb6ef4d07c3fe49d2aea8590c756
nagytommy76 <nagytommy76@gmail.com>
2024-12-28 13:56:28 +0100

    Finished the authorize fn & set session/jwt with user data

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,15 +1,10 @@
 import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
-import { SignJWT, jwtVerify } from 'jose'
-import { MongoDBAdapter } from '@auth/mongodb-adapter'
 
 import { compare } from 'bcrypt'
 import dbConnect from '@DBConnect'
 import UserModel from '@Models/User/User'
 import SignupFormSchema from '@/Validators/SignupFormSchema'
-
-const secretKey = process.env.SESSION_SECRET
-const encodedKey = new TextEncoder().encode(secretKey)
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
    providers: [
@@ -28,28 +23,39 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
                placeholder: 'Jelszó',
             },
          },
-         authorize: async (credentials, request) => {
+         authorize: async (credentials) => {
             const validatedFields = SignupFormSchema().safeParse({
                email: credentials?.email,
                password: credentials?.password,
             })
             const email = validatedFields.data?.email
             const password = validatedFields.data?.password
+            if (!validatedFields.success) return null
 
-            if (validatedFields.success) {
-               await dbConnect()
-               // Search for the user in the database
-               const user = await UserModel.findOne({ email })
-
-               if (!user || !user.password) return null
-
-               if (!(await compare(password as string, user.password as string))) {
-                  return null
-               }
-               // request
-               return user
+            await dbConnect()
+            // Search for the user in the database
+            const user = (await UserModel.findOne({ email }).select([
+               '_id',
+               'userName',
+               'email',
+               'password',
+               'isAdmin',
+               'isEmailConfirmed',
+            ])) as {
+               _id: string
+               userName: string
+               email: string
+               password: string
+               isAdmin: boolean
+               isEmailConfirmed: boolean
             }
-            return null
+
+            if (!user || !user.password) return null
+
+            if (!(await compare(password as string, user.password as string))) {
+               return null
+            }
+            return user
          },
       }),
    ],
@@ -62,36 +68,23 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
          if (user) {
             // User is available during sign-in
             token.id = user.id
-            token
+            token.userName = user.userName
+            token.email = user.email as string
+            token.isAdmin = user.isAdmin
+            token.isEmailConfirmed = user.isEmailConfirmed
          }
          return token
       },
       async session({ session, token }) {
-         console.log(token)
-         session.user.test = 'Kurva anyádat'
+         session.user.id = token.id as string
+         session.user.userName = token.userName as string
+         session.user.email = token.email as string
+         session.user.isAdmin = token.isAdmin as boolean
+         session.user.isEmailConfirmed = token.isEmailConfirmed as boolean
          return session
-      },
-   },
-   jwt: {
-      async encode(params, expirationTime: string | number | Date = '7d'): Promise<string> {
-         // console.log(params)
-         return new SignJWT(params.token)
-            .setProtectedHeader({ alg: 'HS256' })
-            .setIssuedAt()
-            .setExpirationTime(expirationTime)
-            .sign(encodedKey)
       },
    },
    pages: {
       signIn: '/login',
    },
 })
-
-// type JWTPayload = {
-//    email: string
-//    userId: string
-//    userName: string
-//    isAdmin?: boolean
-//    exp?: number
-//    iat?: number
-// }
