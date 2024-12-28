5f1988ea6c00dfc229a92e3b9d7d8751e286742b
nagytommy76 <nagytommy76@gmail.com>
2024-12-28 13:57:17 +0100

    Using auth session to get user data (username etc)

--- components/Navbar/Links/Links.tsx
+++ components/Navbar/Links/Links.tsx
@@ -1,17 +1,13 @@
 import Link from 'next/link'
-import { verifySession } from '@/utils/DataAccessLayer/DataAccessLayer'
-
 import Button from '@mui/material/Button'
 
 import ProfileMenu from '../Menu/ProfileMenu'
+import { auth } from '@NextAuth'
 
 export default async function Links() {
-   const session = await verifySession()
-   const isAuth = session.isAuth
-   const isAdmin = session.isAdmin
-   const userName = session.userName || 'Profil'
+   const session = await auth()
 
-   if (!isAuth)
+   if (!session)
       return (
          <Link href={'/login'}>
             <Button color='inherit' size='large'>
@@ -19,11 +15,13 @@ export default async function Links() {
             </Button>
          </Link>
       )
+   const userName = session.user?.userName
+   const isAdmin = session.user?.isAdmin
 
-   if (isAuth)
+   if (session)
       return (
          <>
-            <ProfileMenu userName={userName} />
+            <ProfileMenu userName={userName || ''} />
             {isAdmin && isAdmin === true && (
                <Button color='inherit' size='large'>
                   Admin oldal
