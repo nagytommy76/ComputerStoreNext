1737fb6b272b0a9b0f0d0e56b47d63dd51c273c8
nagytommy76 <nagytommy76@gmail.com>
2024-12-30 14:15:44 +0100

    Reworked the dbConnect function

--- utils/dbConnect.ts
+++ utils/dbConnect.ts
@@ -1,40 +1,43 @@
+// import mongoose from 'mongoose'
+// declare global {
+//    var mongoose: any // This must be a `var` and not a `let / const`
+// }
+// const MONGODB_URI = process.env.MONGODB_URI!
+
+// if (!MONGODB_URI) {
+//    throw new Error('Please define the MONGODB_URI environment variable')
+// }
+
+// let cached = global.mongoose
+
+// if (!cached) {
+//    cached = global.mongoose = { conn: null, promise: null }
+// }
+
+// async function dbConnect() {
+//    if (cached.conn) return cached.conn
+
+//    if (!cached.promise) {
+//       cached.promise = mongoose.connect(MONGODB_URI, {
+//          bufferCommands: false,
+//       })
+//    }
+//    cached.conn = await cached.promise
+//    return cached.conn
+// }
+
+// export default dbConnect
+
 import mongoose from 'mongoose'
-declare global {
-   var mongoose: any // This must be a `var` and not a `let / const`
-}
-
 const MONGODB_URI = process.env.MONGODB_URI!
-
-if (!MONGODB_URI) {
-   throw new Error('Please define the MONGODB_URI environment variable inside .env.local')
-}
-
-let cached = global.mongoose
-
-if (!cached) {
-   cached = global.mongoose = { conn: null, promise: null }
-}
-
-async function dbConnect() {
-   if (cached.conn) {
-      return cached.conn
-   }
-   if (!cached.promise) {
-      const opts = {
-         bufferCommands: false,
-      }
-      cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-         return mongoose
-      })
-   }
+export default async function dbConnect() {
    try {
-      cached.conn = await cached.promise
-   } catch (e) {
-      cached.promise = null
-      throw e
+      const { connection } = await mongoose.connect(MONGODB_URI as string)
+      if (connection.readyState === 1) {
+         return Promise.resolve(connection)
+      }
+   } catch (error) {
+      console.error(error)
+      return Promise.reject(error)
    }
-
-   return cached.conn
 }
-
-export default dbConnect
