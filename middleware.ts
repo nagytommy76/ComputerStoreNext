0558a79e27e42b757e9ed98ae5487fd5aab33c3b
nagytommy76 <nagytommy76@gmail.com>
2025-01-02 12:03:59 +0100

    Middleware
    Using middleware to protect routes

---
+++ middleware.ts
@@ -0,0 +1,36 @@
+import NextAuth from 'next-auth'
+import authConfig from './utils/auth.config'
+import { authRoutes, protectedRoutes, adminRoutes } from './routes'
+
+const { auth } = NextAuth(authConfig)
+
+export default auth(async function middleware(req) {
+   const nextUrl = req.nextUrl
+   const isLoggedIn = !!req.auth
+   const isAdmin = !!req.auth?.user?.isAdmin
+
+   const isApiAuthRoute = nextUrl.pathname.startsWith('/api/auth')
+   const isProtectedRoute = protectedRoutes.includes(nextUrl.pathname)
+   const isAuthRoute = authRoutes.includes(nextUrl.pathname)
+   const isAdminRoute = adminRoutes.includes(nextUrl.pathname)
+
+   if (isApiAuthRoute) return null
+   if (isAuthRoute) {
+      if (isLoggedIn) return Response.redirect(new URL('/', nextUrl))
+      return null
+   }
+
+   if (!isLoggedIn && isProtectedRoute) return Response.redirect(new URL('/login', nextUrl))
+
+   if (isLoggedIn && !isAdmin && isAdminRoute) return Response.redirect(new URL('/', nextUrl))
+
+   return null
+})
+
+export const config = {
+   matcher: [
+      '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+      // Always run for API routes
+      '/(api|trpc)(.*)',
+   ],
+}
