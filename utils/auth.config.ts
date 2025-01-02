73ffd4addcd37831ce4db65008eb9e030893a432
nagytommy76 <nagytommy76@gmail.com>
2025-01-02 12:07:32 +0100

    Moved providers
    Moved credentials provider from auth.config, to be able to use in the middleware (without database -> edge compability)

--- utils/auth.config.ts
+++ utils/auth.config.ts
@@ -1,66 +1,12 @@
-import CredentialsProvider from 'next-auth/providers/credentials'
-
-import { compare } from 'bcrypt'
-import dbConnect from '@DBConnect'
-import UserModel from '@Models/User/User'
-import SignupFormSchema from '@/Validators/SignupFormSchema'
 import { NextAuthConfig } from 'next-auth'
 
 export default {
-   providers: [
-      CredentialsProvider({
-         id: 'credentials',
-         name: 'credentials',
-         credentials: {
-            email: {
-               label: 'email',
-               type: 'email',
-               placeholder: 'email',
-            },
-            password: {
-               label: 'Jelszó',
-               type: 'password',
-               placeholder: 'Jelszó',
-            },
-         },
-         authorize: async (credentials) => {
-            const validatedFields = SignupFormSchema().safeParse({
-               email: credentials?.email,
-               password: credentials?.password,
-            })
-            const email = validatedFields.data?.email
-            const password = validatedFields.data?.password
-            if (!validatedFields.success) return null
-
-            await dbConnect()
-            // Search for the user in the database
-            const user = (await UserModel.findOne({ email }).select([
-               '_id',
-               'userName',
-               'email',
-               'password',
-               'isAdmin',
-               'isEmailConfirmed',
-            ])) as {
-               _id: string
-               userName: string
-               email: string
-               password: string
-               isAdmin: boolean
-               isEmailConfirmed: boolean
-            }
-
-            if (!user || !user.password) return null
-
-            if (!(await compare(password as string, user.password as string))) {
-               return null
-            }
-            return user
-         },
-      }),
-   ],
+   providers: [],
    session: {
       strategy: 'jwt',
+   },
+   pages: {
+      signIn: '/login',
    },
    callbacks: {
       jwt({ token, user }) {
--- utils/auth.ts
+++ utils/auth.ts
@@ -1,10 +1,66 @@
 import NextAuth from 'next-auth'
 import authConfig from './auth.config'
 
+import CredentialsProvider from 'next-auth/providers/credentials'
+
+import { compare } from 'bcrypt'
+import dbConnect from '@DBConnect'
+import UserModel from '@Models/User/User'
+import SignupFormSchema from '@/Validators/SignupFormSchema'
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
    ...authConfig,
+   providers: [
+      CredentialsProvider({
+         id: 'credentials',
+         name: 'credentials',
+         credentials: {
+            email: {
+               label: 'email',
+               type: 'email',
+               placeholder: 'email',
+            },
+            password: {
+               label: 'Jelszó',
+               type: 'password',
+               placeholder: 'Jelszó',
+            },
+         },
+         authorize: async (credentials) => {
+            const validatedFields = SignupFormSchema().safeParse({
+               email: credentials?.email,
+               password: credentials?.password,
+            })
+            const email = validatedFields.data?.email
+            const password = validatedFields.data?.password
+            if (!validatedFields.success) return null
+
+            await dbConnect()
+            // Search for the user in the database
+            const user = (await UserModel.findOne({ email }).select([
+               '_id',
+               'userName',
+               'email',
+               'password',
+               'isAdmin',
+               'isEmailConfirmed',
+            ])) as {
+               _id: string
+               userName: string
+               email: string
+               password: string
+               isAdmin: boolean
+               isEmailConfirmed: boolean
+            }
+
+            if (!user || !user.password) return null
+
+            if (!(await compare(password as string, user.password as string))) {
+               return null
+            }
+            return user
+         },
+      }),
+   ],
    // https://authjs.dev/guides/extending-the-session
-   pages: {
-      signIn: '/login',
-   },
 })
