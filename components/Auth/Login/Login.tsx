e4fe577055779809f1c921b23202cea113fc7162
nagytommy76 <nagytommy76@gmail.com>
2025-01-03 13:53:34 +0100

    Using components

--- components/Auth/Login/Login.tsx
+++ components/Auth/Login/Login.tsx
@@ -4,38 +4,35 @@ import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 
-import Fade from '@mui/material/Fade'
-import Alert from '@mui/material/Alert'
-
+import InfoBar from './InfoBar'
 import loginAction from '@/serverActions/Login/loginAction'
+import Social from './Social'
 
 import { LoginForm } from './Styles'
 
 export default function Login() {
    const [state, formAction, isPending] = useActionState(loginAction, undefined)
    return (
-      <LoginForm action={formAction}>
-         <Typography variant='h4' align='center'>
-            Bejelentkezés
-         </Typography>
-         <TextField type='email' id='email' name='email' label='Email cím' variant='outlined' fullWidth />
-         <TextField
-            type='password'
-            id='password'
-            name='password'
-            label='Jelszó'
-            variant='outlined'
-            fullWidth
-         />
-         <Button disabled={isPending} type='submit' variant='outlined'>
-            Belépés
-         </Button>
-         <Fade in={state?.errors?.password ? true : false}>
-            <Alert severity='error'>
-               <p>{state?.errors?.password}</p>
-               <p>{state?.errors?.email}</p>
-            </Alert>
-         </Fade>
-      </LoginForm>
+      <>
+         <LoginForm action={formAction}>
+            <Typography variant='h4' align='center'>
+               Bejelentkezés
+            </Typography>
+            <TextField type='email' id='email' name='email' label='Email cím' variant='outlined' fullWidth />
+            <TextField
+               type='password'
+               id='password'
+               name='password'
+               label='Jelszó'
+               variant='outlined'
+               fullWidth
+            />
+            <Button disabled={isPending} type='submit' variant='outlined'>
+               Belépés
+            </Button>
+            <Social />
+         </LoginForm>
+         <InfoBar emailError={state?.errors?.email} passwordError={state?.errors?.password} />
+      </>
    )
 }
--- components/Auth/Login/Styles.tsx
+++ components/Auth/Login/Styles.tsx
@@ -3,7 +3,7 @@ import { styled } from '@mui/material/styles'
 
 export const LoginForm = styled('form')(({ theme }) => ({
    width: '450px',
-   height: '380px',
+   height: '450px',
    borderRadius: '4px',
    padding: '1.7rem',
    backgroundColor: theme.palette.secondary.main,
@@ -12,3 +12,13 @@ export const LoginForm = styled('form')(({ theme }) => ({
    flexDirection: 'column',
    justifyContent: 'space-around',
 }))
+
+export const SocialContainer = styled('div')({
+   width: '100%',
+   margin: '.75rem 0',
+
+   display: 'flex',
+   flexDirection: 'row',
+   justifyContent: 'center',
+   gap: '1rem',
+})
