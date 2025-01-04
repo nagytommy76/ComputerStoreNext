09cd9c83384d2430e56ba838078193ae82525e85
nagytommy76 <nagytommy76@gmail.com>
2025-01-04 12:19:37 +0100

    Delete GoogleProvider
    Removed google provider temporarily

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,8 +1,6 @@
 import NextAuth from 'next-auth'
 import authConfig from './auth.config'
-
 import CredentialsProvider from 'next-auth/providers/credentials'
-import GoogleProvider from 'next-auth/providers/google'
 
 import { compare } from 'bcrypt'
 import dbConnect from '@DBConnect'
@@ -12,17 +10,6 @@ import SignupFormSchema from '@/Validators/SignupFormSchema'
 export const { handlers, signIn, signOut, auth } = NextAuth({
    ...authConfig,
    providers: [
-      GoogleProvider({
-         clientId: process.env.GOOGLE_CLIENT_ID as string,
-         clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-         authorization: {
-            params: {
-               prompt: 'consent',
-               access_type: 'offline',
-               response_type: 'code',
-            },
-         },
-      }),
       CredentialsProvider({
          id: 'credentials',
          name: 'credentials',
