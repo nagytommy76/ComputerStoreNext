436233387139766e172f8d0f6b9a9af987a9657d
nagytommy76 <nagytommy76@gmail.com>
2025-01-06 19:31:05 +0100

    Under production
    Added a tooltip -> function is under development

--- components/Auth/Login/Social.tsx
+++ components/Auth/Login/Social.tsx
@@ -1,10 +1,9 @@
 'use client'
-import { signIn } from 'next-auth/react'
-
 import Image from 'next/image'
 import GoogleIcon from '@images/google.png'
 import FacebookIcon from '@images/facebook.png'
 import Button from '@mui/material/Button'
+import Tooltip from '@mui/material/Tooltip'
 
 import { SocialContainer } from './Styles'
 
@@ -12,17 +11,21 @@ const WIDTH_HEIGHT = 60
 
 export default function Social() {
    const logIn = (provider: 'google' | 'facebook') => {
-      signIn(provider, { callbackUrl: '/' })
+      console.log('FEJLESZTÉS ALATT')
    }
 
    return (
       <SocialContainer>
-         <Button size='large' title='Google bejelentkezés' onClick={() => logIn('google')}>
-            <Image src={GoogleIcon} alt='google' width={WIDTH_HEIGHT} height={WIDTH_HEIGHT} />
-         </Button>
-         <Button size='large' title='Facebook bejelentkezés' onClick={() => logIn('facebook')}>
-            <Image src={FacebookIcon} alt='facebook' width={WIDTH_HEIGHT} height={WIDTH_HEIGHT} />
-         </Button>
+         <Tooltip title='A funkció fejlesztés alatt'>
+            <Button size='large' onClick={() => logIn('google')}>
+               <Image src={GoogleIcon} alt='google' width={WIDTH_HEIGHT} height={WIDTH_HEIGHT} />
+            </Button>
+         </Tooltip>
+         <Tooltip title='A funkció fejlesztés alatt'>
+            <Button size='large' onClick={() => logIn('facebook')}>
+               <Image src={FacebookIcon} alt='facebook' width={WIDTH_HEIGHT} height={WIDTH_HEIGHT} />
+            </Button>
+         </Tooltip>
       </SocialContainer>
    )
 }
