a811e10dd375b91b99aa741c9fb324754d485a3c
nagytommy76 <nagytommy76@gmail.com>
2025-01-15 19:08:07 +0100

    Using profile image

--- components/Navbar/Menu/ProfileMenu.tsx
+++ components/Navbar/Menu/ProfileMenu.tsx
@@ -1,6 +1,9 @@
 'use client'
 import useMenu from './Hook/useMenu'
 
+import Avatar from '@mui/material/Avatar'
+import Image from 'next/image'
+
 import Button from '@mui/material/Button'
 import Divider from '@mui/material/Divider'
 import Menu from '@mui/material/Menu'
@@ -10,12 +13,23 @@ import ProductionQuantityLimitsIcon from '@mui/icons-material/ProductionQuantity
 import LogoutIcon from '@mui/icons-material/Logout'
 
 import logoutAction from '@/serverActions/Logout/Logout'
-export default function ProfileMenu({ userName }: { userName: string }) {
+export default function ProfileMenu({
+   userName,
+   profilePicture,
+}: {
+   userName: string
+   profilePicture?: string
+}) {
    const { open, anchorEl, handleClick, handleClose } = useMenu()
 
    return (
       <>
          <Button aria-expanded={open ? 'true' : undefined} onClick={handleClick} color='inherit' size='large'>
+            {profilePicture && (
+               <Avatar sx={{ mr: 1 }} alt='Remy Sharp'>
+                  <Image src={profilePicture} alt={userName} width={40} height={40} />
+               </Avatar>
+            )}
             {userName}
          </Button>
          <Menu
