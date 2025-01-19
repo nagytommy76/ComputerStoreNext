285353f7ea8a4b5361c9acc3a590952548bd8a1a
nagytommy76 <nagytommy76@gmail.com>
2025-01-19 10:59:35 +0100

    Redirect logout
    Redirect to main page after logout

--- app/actions/Logout/Logout.ts
+++ app/actions/Logout/Logout.ts
@@ -1,13 +1,13 @@
 'use server'
-import { redirect } from 'next/navigation'
-
 import { signOut } from '@NextAuth'
+import { redirect } from 'next/navigation'
 
 export default async function logoutAction() {
    try {
       await signOut({ redirectTo: '/' })
-      redirect('/')
    } catch (error) {
       console.log(error)
+   } finally {
+      redirect('/')
    }
 }
