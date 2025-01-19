117b64c2b341fe4d25a239e755be4e92067fea0d
nagytommy76 <nagytommy76@gmail.com>
2025-01-19 12:23:25 +0100

    Created cart slide

---
+++ components/Navbar/Cart/CartSlide/CartSlide.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { CartSlideContainer } from './Styles'
+
+export default function CartSlide() {
+   return (
+      <CartSlideContainer>
+         <h1>SZLÁLYD</h1>
+      </CartSlideContainer>
+   )
+}
---
+++ components/Navbar/Cart/CartSlide/Styles.ts
@@ -0,0 +1,15 @@
+import { styled } from '@mui/material/styles'
+
+export const CartSlideContainer = styled('div')({
+   position: 'absolute',
+   top: 80,
+   right: 0,
+
+   width: '400px',
+   height: '100vh',
+   backgroundColor: '#666',
+   //    display: 'flex',
+   //    flexDirection: 'column',
+   //    alignItems: 'center',
+   //    justifyContent: 'center',
+})
