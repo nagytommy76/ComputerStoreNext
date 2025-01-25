a43fe5883be98696ebba1651869f1c3732e8b34b
nagytommy76 <nagytommy76@gmail.com>
2025-01-25 13:35:40 +0100

    Responsive menu
    Using responsive menu client component

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -2,31 +2,33 @@ import Link from 'next/link'
 
 import AppBar from '@mui/material/AppBar'
 import Container from '@mui/material/Container'
-import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 
 import { StyledToolbar } from './Styles'
 
-import ThemeToggle from './ThemeToggler/Toggle'
-import NavMenu from './Menu/NavMenu'
+import ResponsiveMenu from './Menu/ResponsiveMenu'
 import Links from './Links/Links'
-
 import Cart from './Cart/Cart'
 
-const Navbar = async () => {
+const Navbar = () => {
    return (
-      <AppBar component={'nav'} position='sticky' color='secondary' elevation={0}>
+      <AppBar
+         component={'nav'}
+         position='fixed'
+         color='secondary'
+         sx={{ backgroundColor: 'rgba(11, 11, 11, 0.7)' }}
+         elevation={0}
+      >
          <Container maxWidth='xl'>
             <StyledToolbar disableGutters>
                <Link href='/'>
-                  <Typography variant='h4'>ComputerStore</Typography>
+                  <Typography color='primary' variant='h4'>
+                     ComputerStore
+                  </Typography>
                </Link>
-               <Box sx={{ display: { xs: 'none', md: 'flex' }, alignItems: 'center' }}>
-                  <NavMenu>
-                     <Links />
-                  </NavMenu>
-                  <ThemeToggle />
-               </Box>
+               <ResponsiveMenu>
+                  <Links />
+               </ResponsiveMenu>
             </StyledToolbar>
          </Container>
          <Cart />
