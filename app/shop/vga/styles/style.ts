462d748e31366bc75fbe32ff2d9b89952c542050
nagytommy76 <nagytommy76@gmail.com>
2025-01-25 13:40:12 +0100

    media query: width to 100%

--- app/shop/vga/styles/style.ts
+++ app/shop/vga/styles/style.ts
@@ -1,8 +1,7 @@
 export const ShopContainerStyle = {
    width: '100%',
    minHeight: '100vh',
-   marginTop: '1.5rem',
-   marginBottom: '1.5rem',
+   marginTop: '100px',
    display: 'flex',
    flexDirection: 'row' as const,
    justifyContent: 'space-between',
