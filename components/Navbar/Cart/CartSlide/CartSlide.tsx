0dc5b30e2c77348bcde50cee727525c66e3d74de
nagytommy76 <nagytommy76@gmail.com>
2025-01-26 12:24:22 +0100

    Working on the cart slide
    Added close icon , Typography and a body section

--- components/Navbar/Cart/CartSlide/CartSlide.tsx
+++ components/Navbar/Cart/CartSlide/CartSlide.tsx
@@ -1,20 +1,26 @@
-import React, { RefObject } from 'react'
+import { CartSlideContainer, CartBodySection } from './Styles'
+import IconButton from '@mui/material/IconButton'
+import CloseIcon from '@mui/icons-material/Close'
+import Typography from '@mui/material/Typography'
 
-import Slide from '@mui/material/Slide'
-import { CartSlideContainer } from './Styles'
-
-export default function CartSlide({
-   isOpen = false,
-   containerRef,
-}: {
-   isOpen: boolean
-   containerRef: RefObject<HTMLElement>
-}) {
+import CartCard from '../CartCard/CartCard'
+export default function CartSlide({ toggleDrawer }: { toggleDrawer: (newOpen: boolean) => () => void }) {
    return (
-      <Slide direction='left' in={isOpen} container={containerRef.current}>
-         <CartSlideContainer>
-            <h1>slide</h1>
-         </CartSlideContainer>
-      </Slide>
+      <CartSlideContainer role='presentation'>
+         <IconButton
+            sx={{ position: 'absolute', top: 0, right: 0 }}
+            size='large'
+            color='warning'
+            onClick={toggleDrawer(false)}
+         >
+            <CloseIcon fontSize='inherit' />
+         </IconButton>
+         <Typography variant='h4' align='center'>
+            Kosár
+         </Typography>
+         <CartBodySection>
+            <CartCard />
+         </CartBodySection>
+      </CartSlideContainer>
    )
 }
