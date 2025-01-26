4b233188d7572382440b332dcccd29ea09c41708
nagytommy76 <nagytommy76@gmail.com>
2025-01-26 12:23:13 +0100

    Added styles
    Added CartSlideContainer & CartBodySection styles

--- components/Navbar/Cart/CartSlide/Styles.ts
+++ components/Navbar/Cart/CartSlide/Styles.ts
@@ -1,15 +1,19 @@
 import { styled } from '@mui/material/styles'
+import Box from '@mui/material/Box'
 
-export const CartSlideContainer = styled('div')({
-   position: 'absolute',
-   top: 80,
-   right: 0,
-
+export const CartSlideContainer = styled(Box)(({ theme }) => ({
+   overflow: 'hidden',
    width: '400px',
-   height: '100vh',
-   backgroundColor: '#666',
-   //    display: 'flex',
-   //    flexDirection: 'column',
-   //    alignItems: 'center',
-   //    justifyContent: 'center',
-})
+   height: '100%',
+   padding: '1rem',
+
+   [`@media (max-width: ${theme.breakpoints.values.sm}px)`]: {
+      width: '100vw',
+   },
+}))
+
+export const CartBodySection = styled('section')(({ theme }) => ({
+   height: '100%',
+   display: 'flex',
+   flexDirection: 'column',
+}))
