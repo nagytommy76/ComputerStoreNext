9879c0ab83c6fac1fec21778e5f134b28c70bf1d
nagytommy76 <nagytommy76@gmail.com>
2025-02-01 10:14:27 +0100

    configure PersistGate

--- utils/redux/StoreProvider.tsx
+++ utils/redux/StoreProvider.tsx
@@ -2,6 +2,7 @@
 import { useRef } from 'react'
 import { Provider } from 'react-redux'
 import { makeStore, AppStore } from './store'
+import { PersistGate } from 'redux-persist/integration/react'
 
 export default function StoreProvider({ children }: { children: React.ReactNode }) {
    const storeRef = useRef<AppStore>()
@@ -10,5 +11,11 @@ export default function StoreProvider({ children }: { children: React.ReactNode 
       storeRef.current = makeStore()
    }
 
-   return <Provider store={storeRef.current}>{children}</Provider>
+   return (
+      <Provider store={storeRef.current}>
+         <PersistGate loading={null} persistor={storeRef.current.__persistor}>
+            {children}
+         </PersistGate>
+      </Provider>
+   )
 }
