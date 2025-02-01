f33d12695e610521b070f061282eb4b1fcbc2e71
nagytommy76 <nagytommy76@gmail.com>
2025-02-01 12:23:27 +0100

    Added Styles to Qty selector

---
+++ components/Navbar/Cart/CartCard/QuantitySelector/Style.tsx
@@ -0,0 +1,14 @@
+import { styled } from '@mui/material/styles'
+
+export const Container = styled('div')({
+   display: 'flex',
+   flexDirection: 'row',
+   alignItems: 'center',
+})
+
+export const QuantityStyle = styled('span')({
+   fontSize: '0.8rem',
+   width: '40px',
+   padding: '0.4rem 0',
+   textAlign: 'center',
+})
