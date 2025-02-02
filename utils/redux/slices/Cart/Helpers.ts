983e93ed90b7d492079879447f72746d21e10c20
nagytommy76 <nagytommy76@gmail.com>
2025-02-02 11:22:48 +0100

    increaseItemQtyByOne helper fn
    increaseItemQtyByOne helper function: found the cart element

--- utils/redux/slices/Cart/Helpers.ts
+++ utils/redux/slices/Cart/Helpers.ts
@@ -1,4 +1,5 @@
-import { CartItemsType, StateType } from './Types'
+import { StateType } from './Types'
+import { CartItemsType } from '@/types/userTypes'
 
 export const checkProductExistsInTheCart = (productId: string, StateCartItems: CartItemsType[]) => {
    return StateCartItems.find((item: CartItemsType) => item.itemId === productId)
@@ -22,4 +23,16 @@ export const calculateTotalPriceAndQuantity = (state: StateType /*stateCartItems
    })
    state.totalQuantity = result.quantity
    state.totalPrice = result.price
+}
+
+export const increaseItemQtyByOne = (state: StateType, id: string, isIncrease: boolean = true) => {
+   const foundElementIndex = searchForStartingIndexInStateCartItems(id, state.cartItems)
+   const foundCartItemInState = checkProductExistsInTheCart(id, state.cartItems)
+   if (foundElementIndex >= 0 && foundCartItemInState !== undefined) {
+      if (isIncrease) foundCartItemInState.quantity++
+      if (!isIncrease && foundCartItemInState.quantity > 1) foundCartItemInState.quantity--
+
+      state.cartItems.splice(foundElementIndex, 1, foundCartItemInState)
+      calculateTotalPriceAndQuantity(state)
+   }
 }
