4fa98a9c06c32726ff52aa41097baaeab3f8a2a9
nagytommy76 <nagytommy76@gmail.com>
2025-02-03 19:07:21 +0100

    Using styles
    Using Cart footer section and body style & pass children

--- components/Navbar/Cart/CartSlide/CartSlide.tsx
+++ components/Navbar/Cart/CartSlide/CartSlide.tsx
@@ -1,10 +1,22 @@
-import { CartSlideContainer, CartBodySection } from './Styles'
+'use client'
+import { useAppSelector } from '@/reduxStore/hooks'
+
+import { CartSlideContainer, CartBodySection, CartFooterSection } from './Styles'
 import IconButton from '@mui/material/IconButton'
-import CloseIcon from '@mui/icons-material/Close'
 import Typography from '@mui/material/Typography'
 
+import CloseIcon from '@mui/icons-material/Close'
 import CartCard from '../CartCard/CartCard'
-export default function CartSlide({ toggleDrawer }: { toggleDrawer: (newOpen: boolean) => () => void }) {
+
+export default function CartSlide({
+   toggleDrawer,
+   children,
+}: {
+   toggleDrawer: (newOpen: boolean) => () => void
+   children: React.ReactNode
+}) {
+   const { cartItems, totalPrice } = useAppSelector((state) => state.cart)
+
    return (
       <CartSlideContainer role='presentation'>
          <IconButton
@@ -15,12 +27,20 @@ export default function CartSlide({ toggleDrawer }: { toggleDrawer: (newOpen: bo
          >
             <CloseIcon fontSize='inherit' />
          </IconButton>
-         <Typography variant='h4' align='center'>
+         <Typography variant='h4' align='center' marginBottom={'2rem'}>
             Kosár
          </Typography>
          <CartBodySection>
-            <CartCard />
+            {cartItems.map((singleCartItem) => (
+               <CartCard singleCartItem={singleCartItem} key={singleCartItem.itemId} />
+            ))}
          </CartBodySection>
+         <CartFooterSection>
+            <Typography variant='h6' align='left' marginBottom={'2rem'}>
+               Összesen: {totalPrice.toLocaleString()} Ft
+            </Typography>
+            {children}
+         </CartFooterSection>
       </CartSlideContainer>
    )
 }
