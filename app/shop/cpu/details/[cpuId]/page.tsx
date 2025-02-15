a10c9c0b052650ef6b78f6d2d68cd9e33745e411
nagytommy76 <nagytommy76@gmail.com>
2025-02-15 13:50:46 +0100

    Created CPU details page
    Created and using DetailsPage component

---
+++ app/shop/cpu/details/[cpuId]/page.tsx
@@ -0,0 +1,36 @@
+import { Metadata } from 'next'
+import { CpuType } from '@Models/Cpu/cpuTypes'
+
+import DetailsPage from '@/Details/DetailsPage'
+import CpuDetailTable from '@/Details/ProductsTable/Cpu/CpuDetailTable'
+
+type Props = {
+   searchParams: Promise<{ productMan: string; productType: string; typeCode: string }>
+}
+
+export async function generateMetadata({ searchParams }: Props): Promise<Metadata> {
+   const { productMan, productType, typeCode } = await searchParams
+   return {
+      title: `${productMan} ${productType} ${typeCode} | ComputerStore`,
+      description: `${productMan} ${productType} ${typeCode}`,
+   }
+}
+
+async function getCpuData(id: string) {
+   const response = await fetch(`${process.env.APP_URL}/api/cpu/details?cpuId=${id}`, {
+      method: 'GET',
+   })
+   return (await response.json()) as Promise<{ foundCpuDetails: CpuType }>
+}
+
+export default async function page({ params }: { params: Promise<{ cpuId: string }> }) {
+   const cpuId = (await params).cpuId
+   const { foundCpuDetails } = await getCpuData(cpuId)
+
+   return (
+      <DetailsPage
+         details={foundCpuDetails}
+         DetailsTableComponent={<CpuDetailTable details={foundCpuDetails.details} />}
+      />
+   )
+}
