e7ffe0d5b9d7b30adfa38ca5ab42ab78d21a13b2
nagytommy76 <nagytommy76@gmail.com>
2025-02-15 13:51:54 +0100

    Using DetailsPage component

--- app/shop/vga/details/[vgaId]/page.tsx
+++ app/shop/vga/details/[vgaId]/page.tsx
@@ -1,20 +1,13 @@
 import { Metadata } from 'next'
-import dynamic from 'next/dynamic'
 import { VgaType } from '@/models/Vga/vgaTypes'
 
-import { HeadSection, DetailsContainer } from './styles'
-
-import RightHead from '@/Details/RightHead/RightHead'
-import TopNavigation from '@/Details/TopNavigation/TopNavigation'
-import BodySection from '@/Details/Body/Body'
+import DetailsPage from '@/Details/DetailsPage'
 import VgaDetailsTable from '@/Details/ProductsTable/Vga/VgaDetailsTable'
-import Chart from '@/Details/Chart/Chart'
-import Rating from '@/Details/Rating/Rating'
-const ImageSlider = dynamic(() => import('@/Details/ImageSlider/ImageSlider'))
 
 type Props = {
    searchParams: Promise<{ productMan: string; productType: string; typeCode: string }>
 }
+
 export async function generateMetadata({ searchParams }: Props): Promise<Metadata> {
    const { productMan, productType, typeCode } = await searchParams
    return {
@@ -35,30 +28,9 @@ export default async function page({ params }: { params: Promise<{ vgaId: string
    const { foundVgaDetails } = await getVgaData(vgaId.vgaId)
 
    return (
-      <DetailsContainer>
-         <TopNavigation productType='vga' type={foundVgaDetails.type} />
-         <HeadSection>
-            <ImageSlider
-               pictureUrls={foundVgaDetails.pictureUrls}
-               type={foundVgaDetails.type}
-               typeCode={foundVgaDetails.typeCode as string}
-               manufacturer={foundVgaDetails.manufacturer}
-            />
-            <RightHead
-               manufacturer={foundVgaDetails.manufacturer}
-               type={foundVgaDetails.type}
-               typeCode={foundVgaDetails.typeCode as string}
-               price={foundVgaDetails.price}
-               warranty={foundVgaDetails.details.warranity}
-               manufacturerPageUrl={foundVgaDetails.details.manufacturerPageUrl}
-            />
-         </HeadSection>
-         <BodySection
-            description={foundVgaDetails.details.description}
-            ProductDetailsTable={<VgaDetailsTable vgaDetails={foundVgaDetails.details} />}
-         />
-         <Chart chartData={foundVgaDetails.details.chartData} />
-         <Rating comments={foundVgaDetails.ratingValues} />
-      </DetailsContainer>
+      <DetailsPage
+         details={foundVgaDetails}
+         DetailsTableComponent={<VgaDetailsTable vgaDetails={foundVgaDetails.details} />}
+      />
    )
 }
