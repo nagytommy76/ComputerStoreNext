b095b37ab15ba9332d06706a2117414e94d9f252
nagytommy76 <nagytommy76@gmail.com>
2025-02-16 11:08:05 +0100

    Using details suspense

---
+++ app/shop/cpu/details/[cpuId]/loading.tsx
@@ -0,0 +1,7 @@
+'use client'
+import DetailsPageSuspense from '@/Suspense/DetailsPage/DetailsPage'
+
+export default function Loading() {
+   // You can add any UI inside Loading, including a Skeleton.
+   return <DetailsPageSuspense />
+}
