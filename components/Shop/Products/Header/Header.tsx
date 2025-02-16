12ab12f5fe35a2c8264048b367633b326460a54a
nagytommy76 <nagytommy76@gmail.com>
2025-02-16 11:48:28 +0100

    Add styles & media query
    Using styled component & added media query

--- components/Shop/Products/Header/Header.tsx
+++ components/Shop/Products/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { styled } from '@mui/material/styles'
 import Typography from '@mui/material/Typography'
 import { useAppSelector } from '@/reduxStore/hooks'
 
@@ -6,22 +7,27 @@ import SearchField from './SearchField'
 export default function Header({ productName }: { productName: string }) {
    const totalProducts = useAppSelector((state) => state.filter.totalProducts)
    return (
-      <div
-         style={{
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            gap: '1rem',
-            marginBottom: '1.5rem',
-         }}
-      >
-         <Typography variant='h3' gutterBottom>
-            {productName}
-         </Typography>
-         <Typography variant='body2' color='text.secondary' gutterBottom>
+      <StyledContainer>
+         <Typography variant='h3'>{productName}</Typography>
+         <Typography variant='body2' color='text.secondary'>
             ({totalProducts} termék)
          </Typography>
          <SearchField />
-      </div>
+      </StyledContainer>
    )
 }
+
+const StyledContainer = styled('div')(({ theme }) => ({
+   display: 'flex',
+   flexDirection: 'row',
+   alignItems: 'center',
+   gap: '1rem',
+   marginBottom: '1.5rem',
+
+   [`@media (max-width: ${theme.breakpoints.values.sm}px)`]: {
+      flexDirection: 'column',
+      marginTop: '1rem',
+      marginBottom: '2rem',
+      gap: '0.5rem',
+   },
+}))
