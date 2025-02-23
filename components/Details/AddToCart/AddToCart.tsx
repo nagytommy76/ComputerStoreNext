7b3dfd61aa00ce9972c57ecfd397eb8975125217
nagytommy76 <nagytommy76@gmail.com>
2025-02-23 12:11:29 +0100

    Add to cart
    Finished the add to cart functionality

--- components/Details/AddToCart/AddToCart.tsx
+++ components/Details/AddToCart/AddToCart.tsx
@@ -1,11 +1,42 @@
 'use client'
-import React from 'react'
+import { useState } from 'react'
+import { useAppDispatch } from '@/reduxStore/hooks'
+import { addToCart } from '@/reduxStore/slices/Cart/CartSlice'
+
 import { StyledCartSection, StyledNumberInput } from './Styles'
 
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart'
 import IconButton from '@mui/material/IconButton'
 
-export default function AddToCart() {
+export default function AddToCart({
+   manufacturer,
+   price,
+   type,
+   productType,
+   productId,
+   pictureUrl,
+}: {
+   manufacturer: string
+   type: string
+   price: number
+   productType: string
+   pictureUrl: string
+   productId: string
+}) {
+   const [quantity, setQuantity] = useState(1)
+   const dispatch = useAppDispatch()
+   const handleAddToCart = () =>
+      dispatch(
+         addToCart({
+            _id: productId,
+            displayImage: pictureUrl,
+            displayName: manufacturer + ' ' + type,
+            itemQuantity: quantity,
+            productType,
+            price: price,
+         })
+      )
+
    return (
       <StyledCartSection>
          <StyledNumberInput
@@ -15,17 +46,12 @@ export default function AddToCart() {
             id='qty'
             variant='filled'
             label='Darab'
-            value={1}
-            onChange={() => {}}
-         />
-         <IconButton
-            size='large'
-            onClick={() => {
-               console.log('KLIKK KORÁRBA')
+            value={quantity}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+               setQuantity(Number(event.target.value))
             }}
-            color='primary'
-            aria-label='add to shopping cart'
-         >
+         />
+         <IconButton size='large' onClick={handleAddToCart} color='primary' aria-label='add to shopping cart'>
             <AddShoppingCartIcon fontSize='large' />
          </IconButton>
       </StyledCartSection>
