07549174fd6c1f77c6fb2e2aabd201a71486f53d
nagytommy76 <nagytommy76@gmail.com>
2025-02-26 19:52:24 +0100

    Moved vgaFilter types

--- components/Shop/Vga/types.ts
+++ components/Shop/Vga/types.ts
@@ -9,21 +9,3 @@ export interface VgaFilterSlice {
    selectedPowerConsuption: number[]
    selectedLength: number[]
 }
-
-export interface VgaFilterType {
-   gpuManufacturer: string[]
-   pciType: string[]
-   vramType: string[]
-   minBaseClock: number
-   maxBaseClock: number
-   minBoostClock: number
-   maxBoostClock: number
-   minLength: number
-   maxLength: number
-   minTdp: number
-   maxTdp: number
-   minVramBandwidth: number
-   maxVramBandwidth: number
-   minVramCapacity: number
-   maxVramCapacity: number
-}
