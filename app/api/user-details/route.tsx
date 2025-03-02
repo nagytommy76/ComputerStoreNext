5214a36cc86a3d1ab242ad64d93e1b1daa586eb0
nagytommy76 <nagytommy76@gmail.com>
2025-03-02 12:25:24 +0100

    Find users
    Find User or GoogleUser

--- app/api/user-details/route.tsx
+++ app/api/user-details/route.tsx
@@ -1,8 +1,22 @@
-import { auth } from '@NextAuth'
-import { NextResponse } from 'next/server'
-// import dbConnect from '@DBConnect'
+import { NextResponse, NextRequest } from 'next/server'
+import { convertSearchParamsToQueryObject } from '@/services/BaseProductRoute'
+import dbConnect from '@DBConnect'
 
-export async function GET() {
-   const auth1 = await auth()
-   return NextResponse.json({ auth: auth1 }, { status: 200 })
+import User from '@Models/User/User'
+import GoogleUser from '@Models/User/GoogleUser'
+
+export async function GET(request: NextRequest) {
+   const paramsObject = convertSearchParamsToQueryObject(request.nextUrl.searchParams)
+   const email = paramsObject.email
+   await dbConnect()
+
+   const user = await User.findOne({ email }).select(['userDetails', 'email', 'userName'])
+   if (!user) {
+      const googleUser = await GoogleUser.findOne({ email }).select(['userDetails', 'email', 'name'])
+      if (googleUser) {
+         return NextResponse.json({ user: googleUser }, { status: 200 })
+      }
+   }
+
+   return NextResponse.json({ user }, { status: 200 })
 }
