8f54e9f72e16aa143980f751a2eb70b4d8b29a0d
nagytommy76 <nagytommy76@gmail.com>
2025-03-02 12:29:01 +0100

    Add CheckoutContainer style

---
+++ components/Checkout/Styles.ts
@@ -0,0 +1,12 @@
+'use client'
+import { styled } from '@mui/material/styles'
+
+export const CheckoutContainer = styled('section')({
+   minHeight: '100vh',
+   margin: '110px auto 0 auto',
+
+   display: 'flex',
+   alignItems: 'center',
+   justifyContent: 'center',
+   width: '100%',
+})
