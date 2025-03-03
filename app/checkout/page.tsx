a233e182452457503f175712cbe7506372feef87
nagytommy76 <nagytommy76@gmail.com>
2025-03-03 18:34:26 +0100

    Fetch call
    Created getUserDetails & using auth session

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,22 +1,33 @@
+import { UserDetailsTypes } from '@/types/userTypes'
+import { auth } from '@NextAuth'
 import { Metadata } from 'next'
+
+import StepsContainer from '@Checkout/Steps/StepsContainer'
+import { CheckoutContainer } from '@Checkout/Styles'
 
 export const metadata: Metadata = {
    title: 'Computer Store | Rendelés leadása',
    description: 'Checkout page',
 }
 
-async function getUserDetails() {
-   const data = await fetch(`${process.env.APP_URL}/api/user-details`, { method: 'GET' })
-   const response = await data.json()
+async function getUserDetails(email: string) {
+   const data = await fetch(`${process.env.APP_URL}/api/user-details?email=${email}`, {
+      method: 'GET',
+   })
+   const response = (await data.json()) as Promise<{
+      user: { userDetails: UserDetailsTypes | undefined; name?: string; userName?: string; email: string }
+   }>
    return await response
 }
 
 export default async function page() {
-   const response = await getUserDetails()
-   console.log(response)
+   const session = await auth()
+   const { user } = await getUserDetails(session?.user?.email || '')
+
    return (
-      <div>
+      <CheckoutContainer>
+         <StepsContainer userDetails={user.userDetails} />
          <h1>CHECKOUT</h1>
-      </div>
+      </CheckoutContainer>
    )
 }
