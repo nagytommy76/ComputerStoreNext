42f8cc7cd2f5007b4e08c5768c2b331babb0cd07
nagytommy76 <nagytommy76@gmail.com>
2025-03-04 19:53:17 +0100

    Using style
    Using StepsContainerStyle & removed useTooltip -> get userDetails as props

--- components/Checkout/Steps/StepsContainer.tsx
+++ components/Checkout/Steps/StepsContainer.tsx
@@ -1,19 +1,29 @@
 'use client'
 import useSteps from '../Hooks/useSteps'
+
 import StepHeader from './StepHeader'
+import { StepsContainerStyle } from '../Styles'
 
 import type { UserDetailsTypes } from '@/types/userTypes'
 
-export default function StepsContainer({ userDetails }: { userDetails: UserDetailsTypes | undefined }) {
+export default function StepsContainer({
+   userDetails,
+   stepComponents,
+}: {
+   userDetails: UserDetailsTypes | undefined
+   stepComponents: JSX.Element[]
+}) {
    const { currentStep, nextStep, prevStep } = useSteps()
+
    return (
-      <div>
+      <StepsContainerStyle>
          <StepHeader
-            userDetails={userDetails}
+            isUserDetailsFilled={userDetails ? true : false}
             currentStep={currentStep}
             nextStep={nextStep}
             prevStep={prevStep}
          />
-      </div>
+         <div style={{ minHeight: '50vh' }}>{stepComponents[currentStep]}</div>
+      </StepsContainerStyle>
    )
 }
