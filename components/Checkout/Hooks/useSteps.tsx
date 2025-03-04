52caf78e83d3f2aab9fbc89fcc88af8fed752ea5
nagytommy76 <nagytommy76@gmail.com>
2025-03-04 19:52:21 +0100

    If check
    Check if current step is 3 or 0

--- components/Checkout/Hooks/useSteps.tsx
+++ components/Checkout/Hooks/useSteps.tsx
@@ -4,10 +4,12 @@ export default function useSteps() {
    const [currentStep, setCurrentStep] = useState<number>(0)
 
    const nextStep = () => {
+      if (currentStep === 3) return
       setCurrentStep(currentStep + 1)
    }
 
    const prevStep = () => {
+      if (currentStep === 0) return
       setCurrentStep(currentStep - 1)
    }
 
