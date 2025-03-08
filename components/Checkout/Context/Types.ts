d93fa45f0675a637ed115d64b590d9ceea2f5cab
nagytommy76 <nagytommy76@gmail.com>
2025-03-08 12:10:43 +0100

    Add Checkout reducer/context types

---
+++ components/Checkout/Context/Types.ts
@@ -0,0 +1,38 @@
+import type { UserDetailsTypes } from '@/types/userTypes'
+
+export interface ISetUserDetails {
+   type: 'SET_USER_DETAILS'
+   payload: UserDetailsTypes
+}
+export interface ISetDetailField {
+   type: 'SET_DETAIL_FIELD'
+   payload: { field: string; value: string }
+}
+
+export type ICheckoutAction = ISetUserDetails | ISetDetailField
+
+export interface ICheckoutState {
+   userDetails: UserDetailsTypes
+}
+
+// CONTEXT
+export interface ICheckoutContext {
+   checkoutReducer: ICheckoutState
+   checkoutDispatch: React.Dispatch<ICheckoutAction>
+}
+
+export const checkoutData: ICheckoutState = {
+   userDetails: {
+      firstName: '',
+      lastName: '',
+      phone: '',
+      address: {
+         zipCode: 0,
+         city: '',
+         street: '',
+         houseNumber: '',
+         floor: '',
+         door: '',
+      },
+   },
+}
