249a234bb5675901dfd1ba82cc70e6b7e4747673
nagytommy76 <nagytommy76@gmail.com>
2025-03-08 12:12:09 +0100

    Add 2 styles

---
+++ components/Checkout/Steps/AdressForm/Styles.ts
@@ -0,0 +1,17 @@
+'use client'
+import { styled } from '@mui/material/styles'
+
+export const AdressFromStyle = styled('div')({
+   width: '100%',
+   display: 'flex',
+   flexDirection: 'column',
+   alignItems: 'center',
+   justifyContent: 'center',
+})
+
+export const FormControlRow = styled('div')({
+   //    width: '85%',
+   display: 'flex',
+   flexDirection: 'row',
+   justifyContent: 'space-evenly',
+})
