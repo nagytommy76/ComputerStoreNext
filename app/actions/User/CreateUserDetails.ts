4ec4ab4f4dccaac88ace70925862108f84d4a44d
nagytommy76 <nagytommy76@gmail.com>
2025-03-22 11:39:14 +0100

    Zod object modified
    removed address

--- app/actions/User/CreateUserDetails.ts
+++ app/actions/User/CreateUserDetails.ts
@@ -11,14 +11,12 @@ const schema = z.object({
    firstName: z.string().min(1),
    lastName: z.string().min(1),
    phone: z.string().regex(/^\+36(20|30|70)\d{7}$/, 'Helytelen telefonszám fromátum: +36(20|30|70)XXXXXXX'),
-   address: z.object({
-      zipCode: z.string().regex(/^(1[1-9]\d{2}|[2-9]\d{3})$/, 'Helytelen irányítószám! Formátum: 1111-9999'),
-      city: z.string().min(1),
-      street: z.string().min(1),
-      houseNumber: z.string().min(1),
-      floor: z.string(),
-      door: z.string(),
-   }),
+   zipCode: z.string().regex(/^(1[1-9]\d{2}|[2-9]\d{3})$/, 'Helytelen irányítószám! Formátum: 1111-9999'),
+   city: z.string().min(1),
+   street: z.string().min(1),
+   houseNumber: z.string().min(1),
+   floor: z.string(),
+   door: z.string(),
 })
 
 export default async function CreateUserDetails(email: string, formData: FormData) {
