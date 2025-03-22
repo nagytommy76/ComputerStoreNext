0c05897673c84c73e895eb14ce96acae61d54873
nagytommy76 <nagytommy76@gmail.com>
2025-03-22 11:38:04 +0100

    useError hook
    Created IErrorFields & set the error msgs -> setErrorsHandler fn

---
+++ components/Checkout/Steps/AdressForm/Hooks/useErrors.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+interface IErrorFields {
+   firstName?: undefined | string[]
+   lastName?: undefined | string[]
+   phone?: undefined | string[]
+   zipCode?: undefined | string[]
+   city?: undefined | string[]
+   street?: undefined | string[]
+   houseNumber?: undefined | string[]
+   floor?: undefined | string[]
+   door?: undefined | string[]
+}
+
+export default function useErrors() {
+   const [errors, setErrors] = useState<IErrorFields>({
+      firstName: undefined,
+      lastName: undefined,
+      phone: undefined,
+      zipCode: undefined,
+      city: undefined,
+      street: undefined,
+      houseNumber: undefined,
+   })
+
+   function setErrorsHandler(errors: IErrorFields) {
+      setErrors({
+         firstName: errors.firstName,
+         lastName: errors.lastName,
+         phone: errors.phone,
+         zipCode: errors.zipCode,
+         city: errors.city,
+         street: errors.street,
+         houseNumber: errors.houseNumber,
+      })
+   }
+
+   return { errors, setErrorsHandler }
+}
