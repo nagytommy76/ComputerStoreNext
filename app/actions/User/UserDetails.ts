12796e914bd9e3f6d6fdd18a00ae16e7a47e6b8a
nagytommy76 <nagytommy76@gmail.com>
2025-03-22 11:39:37 +0100

    Removed address field

--- app/actions/User/UserDetails.ts
+++ app/actions/User/UserDetails.ts
@@ -1,17 +1,13 @@
-import type { UserDetailsTypes } from '@/types/userTypes'
-
-export function extractFormData(formData: FormData): UserDetailsTypes {
+export function extractFormData(formData: FormData) {
    return {
       firstName: formData.get('firstName') as string,
       lastName: formData.get('lastName') as string,
       phone: formData.get('phone') as string,
-      address: {
-         zipCode: formData.get('zipCode') as string,
-         city: formData.get('city') as string,
-         street: formData.get('street') as string,
-         houseNumber: formData.get('houseNumber') as string,
-         floor: formData.get('floor') as string,
-         door: formData.get('door') as string,
-      },
+      zipCode: formData.get('zipCode') as string,
+      city: formData.get('city') as string,
+      street: formData.get('street') as string,
+      houseNumber: formData.get('houseNumber') as string,
+      floor: formData.get('floor') as string,
+      door: formData.get('door') as string,
    }
 }
