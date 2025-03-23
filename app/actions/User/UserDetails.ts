b7f456adcda542e584b24df2c3c777b10010a387
nagytommy76 <nagytommy76@gmail.com>
2025-03-23 11:05:03 +0100

    Add validatedFormData function

--- app/actions/User/UserDetails.ts
+++ app/actions/User/UserDetails.ts
@@ -1,3 +1,5 @@
+import { SafeParseSuccess } from 'zod'
+
 export function extractFormData(formData: FormData) {
    return {
       firstName: formData.get('firstName') as string,
@@ -9,5 +11,33 @@ export function extractFormData(formData: FormData) {
       houseNumber: formData.get('houseNumber') as string,
       floor: formData.get('floor') as string,
       door: formData.get('door') as string,
+   }
+}
+
+export function validatedFormData(
+   validatedFields: SafeParseSuccess<{
+      firstName: string
+      lastName: string
+      phone: string
+      zipCode: string
+      city: string
+      street: string
+      houseNumber: string
+      floor: string
+      door: string
+   }>
+) {
+   return {
+      firstName: validatedFields.data.firstName,
+      lastName: validatedFields.data.lastName,
+      phone: validatedFields.data.phone,
+      address: {
+         zipCode: validatedFields.data.zipCode,
+         city: validatedFields.data.city,
+         street: validatedFields.data.street,
+         houseNumber: validatedFields.data.houseNumber,
+         floor: validatedFields.data.floor,
+         door: validatedFields.data.door,
+      },
    }
 }
