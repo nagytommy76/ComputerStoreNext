0769d76b2d9204583bed96bced8eb5e4022baa08
nagytommy76 <nagytommy76@gmail.com>
2025-03-23 11:04:40 +0100

    Using AddressForm
    Using validatedFormData & addres form function

--- app/actions/User/CreateUserDetails.ts
+++ app/actions/User/CreateUserDetails.ts
@@ -1,28 +1,15 @@
 'use server'
-import { z } from 'zod'
 import dbConnect from '@DBConnect'
 import UserModel from '@Models/User/User'
 import GoogleUserModel from '@Models/User/GoogleUser'
 import { revalidatePath } from 'next/cache'
 
-import { extractFormData } from './UserDetails'
-
-const schema = z.object({
-   firstName: z.string().min(1),
-   lastName: z.string().min(1),
-   phone: z.string().regex(/^\+36(20|30|70)\d{7}$/, 'Helytelen telefonszám fromátum: +36(20|30|70)XXXXXXX'),
-   zipCode: z.string().regex(/^(1[1-9]\d{2}|[2-9]\d{3})$/, 'Helytelen irányítószám! Formátum: 1111-9999'),
-   city: z.string().min(1),
-   street: z.string().min(1),
-   houseNumber: z.string().min(1),
-   floor: z.string(),
-   door: z.string(),
-})
+import { extractFormData, validatedFormData } from './UserDetails'
+import AddressForm from '@/Validators/AddressForm'
 
 export default async function CreateUserDetails(email: string, formData: FormData) {
    const userDetails = extractFormData(formData)
-   const validatedFields = schema.safeParse(userDetails)
-
+   const validatedFields = AddressForm().safeParse(userDetails)
    if (!validatedFields.success) {
       return {
          errors: validatedFields.error.flatten().fieldErrors,
@@ -34,10 +21,10 @@ export default async function CreateUserDetails(email: string, formData: FormDat
    const foundUser = await UserModel.findOne({ email }).select(['userDetails'])
    if (!foundUser) {
       const foundGoogleUser = await GoogleUserModel.findOne({ email }).select(['userDetails'])
-      foundGoogleUser.userDetails = validatedFields
+      foundGoogleUser.userDetails = validatedFormData(validatedFields)
       await foundGoogleUser.save()
    } else {
-      foundUser.userDetails = validatedFields
+      foundUser.userDetails = validatedFormData(validatedFields)
       await foundUser.save()
    }
 
