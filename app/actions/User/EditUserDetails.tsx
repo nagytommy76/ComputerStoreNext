db8e1bd60fec09f6d9f9cdf42904258c95e7ba71
nagytommy76 <nagytommy76@gmail.com>
2025-03-25 19:41:42 +0100

    Using updateOne
    Update userDetails & using switch/case

--- app/actions/User/EditUserDetails.tsx
+++ app/actions/User/EditUserDetails.tsx
@@ -4,10 +4,48 @@ import UserModel from '@Models/User/User'
 import GoogleUserModel from '@Models/User/GoogleUser'
 import { revalidatePath } from 'next/cache'
 
-import { extractFormData } from './UserDetails'
+import { extractFormData, validatedFormData } from './UserDetails'
+import AddressForm from '@/Validators/AddressForm'
+import type { ProviderType } from '@/types/userTypes'
 
-export default async function EditUserDetails(email: string, formData: FormData) {
+export default async function EditUserDetails(email: string, authProvider: ProviderType, formData: FormData) {
    const userDetails = extractFormData(formData)
+   const validatedFields = AddressForm().safeParse(userDetails)
+   if (!validatedFields.success) {
+      return {
+         errors: validatedFields.error.flatten().fieldErrors,
+      }
+   }
 
    await dbConnect()
+
+   switch (authProvider) {
+      case 'google':
+         await GoogleUserModel.updateOne(
+            {
+               email,
+            },
+            {
+               $set: {
+                  userDetails: validatedFormData(validatedFields),
+               },
+            }
+         )
+         break
+      default:
+         await UserModel.updateOne(
+            {
+               email,
+            },
+            {
+               $set: {
+                  userDetails: validatedFormData(validatedFields),
+               },
+            }
+         )
+         break
+   }
+   revalidatePath('/checkout')
+
+   return 200
 }
