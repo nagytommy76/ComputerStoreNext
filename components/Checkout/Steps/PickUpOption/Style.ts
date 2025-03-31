9f22a0b5829713b4b7269309eba9d715ebeb3447
nagytommy76 <nagytommy76@gmail.com>
2025-03-31 19:36:55 +0200

    Add StyledPaper stle

---
+++ components/Checkout/Steps/PickUpOption/Style.ts
@@ -0,0 +1,7 @@
+import { styled } from '@mui/material/styles'
+import Paper from '@mui/material/Paper'
+
+export const StyledPaper = styled(Paper)({
+   margin: '.6rem 0',
+   padding: '.4rem 1rem',
+})
