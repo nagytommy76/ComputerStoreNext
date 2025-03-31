3d9bf273a0524466ecd63a1a7966053014fb84df
nagytommy76 <nagytommy76@gmail.com>
2025-03-31 19:38:37 +0200

    Using pickUp
    Add pickUp & provider

--- components/Checkout/Steps/StepsContainer.tsx
+++ components/Checkout/Steps/StepsContainer.tsx
@@ -1,9 +1,12 @@
 'use client'
 import useSteps from '../Hooks/useSteps'
 import CheckoutContextProvider from '../Context/CheckoutContext'
+import type { ProviderType } from '@/types/userTypes'
 
 import StepHeader from './StepHeader'
 import AdressForm from './AdressForm/AdressForm'
+import PickUp from './PickUpOption/PickUp'
+
 import { StepsContainerStyle } from '../Styles'
 import type { UserDetailsTypes } from '@/types/userTypes'
 
@@ -14,17 +17,17 @@ export default function StepsContainer({
 }: {
    userDetails: UserDetailsTypes | undefined
    email: string
-   provider?: 'google' | 'facebook' | 'credentials'
+   provider?: ProviderType
 }) {
    const { currentStep, nextStep, prevStep } = useSteps()
 
-   const stepComponents = [<AdressForm key={0} />]
+   const stepComponents = [<AdressForm key={0} />, <PickUp key={1} />]
 
    return (
       <StepsContainerStyle>
          <CheckoutContextProvider provider={provider} userDetails={userDetails} email={email}>
             <StepHeader currentStep={currentStep} nextStep={nextStep} prevStep={prevStep} />
-            <div style={{ minHeight: '50vh' }}>{stepComponents[currentStep]}</div>
+            <div style={{ minHeight: '50vh', width: '100%' }}>{stepComponents[currentStep]}</div>
          </CheckoutContextProvider>
       </StepsContainerStyle>
    )
