4d1e8ac6c95c70bb725bdfc7f3ab88517877b601
nagytommy76 <nagytommy76@gmail.com>
2025-04-01 19:47:43 +0200

    Add PickUpContainer style

--- components/Checkout/Steps/PickUpOption/Style.ts
+++ components/Checkout/Steps/PickUpOption/Style.ts
@@ -1,6 +1,14 @@
 import { styled } from '@mui/material/styles'
 import Paper from '@mui/material/Paper'
 
+export const PickUpContainer = styled('div')({
+   width: '100%',
+   height: '100%',
+   display: 'flex',
+   justifyContent: 'center',
+   alignItems: 'center',
+})
+
 export const StyledPaper = styled(Paper)({
    margin: '.6rem 0',
    padding: '.4rem 1rem',
