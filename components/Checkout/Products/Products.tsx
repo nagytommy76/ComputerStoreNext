e4920de736879e303aa3a3b8bcf69c75fd758c23
nagytommy76 <nagytommy76@gmail.com>
2025-04-06 11:57:57 +0200

    Using ItemsContainter & PriceFooter
    With SSR disabled

--- components/Checkout/Products/Products.tsx
+++ components/Checkout/Products/Products.tsx
@@ -1,17 +1,18 @@
 'use client'
-import { useAppSelector } from '@/reduxStore/hooks'
+import dynamic from 'next/dynamic'
+
+const ItemsContainter = dynamic(() => import('./Includes/ItemsContainter'), { ssr: false })
+const PriceFooter = dynamic(() => import('./Includes/PriceFooter'), { ssr: false })
 
 import BackgroundImage from '@images/AdressBackG.jpg'
 import { ProductContainer, ProductImage } from './Styles'
 
 export default function Products() {
-   const cartItems = useAppSelector((state) => state.cart.cartItems)
-   const totalPrice = useAppSelector((state) => state.cart.totalPrice)
-
    return (
       <ProductContainer>
          <ProductImage src={BackgroundImage} alt='background' width={900} height={1500} />
-         <h1>SDADSSDASDA</h1>
+         <ItemsContainter />
+         <PriceFooter />
       </ProductContainer>
    )
 }
