81280739ba2e8177abbe99e1daa95fd942410511
nagytommy76 <nagytommy76@gmail.com>
2025-04-06 11:58:30 +0200

    Add styles
    Add ItemsContainer & FooterSection & PriceSection styles

--- components/Checkout/Products/Styles.ts
+++ components/Checkout/Products/Styles.ts
@@ -1,6 +1,7 @@
 'use client'
 import Image from 'next/image'
 import { styled } from '@mui/material/styles'
+import Typography from '@mui/material/Typography'
 
 export const ProductContainer = styled('div')({
    position: 'relative',
@@ -10,6 +11,7 @@ export const ProductContainer = styled('div')({
    flexDirection: 'column',
    alignItems: 'center',
    justifyContent: 'center',
+   backgroundColor: 'rgba(10, 10, 10, 0.5)',
 })
 
 export const ProductImage = styled(Image)({
@@ -22,3 +24,37 @@ export const ProductImage = styled(Image)({
    height: '100%',
    objectFit: 'cover',
 })
+
+export const ItemsContainer = styled('div')({
+   width: '400px',
+   height: '75vh',
+   overflow: 'scroll',
+
+   display: 'flex',
+   flexDirection: 'column',
+   alignItems: 'center',
+   gap: '1.25rem',
+})
+
+export const FooterSection = styled('footer')({
+   width: '100%',
+   marginTop: '1rem',
+
+   display: 'flex',
+   flexDirection: 'column',
+   alignItems: 'center',
+   justifyContent: 'space-between',
+})
+
+export const PriceSection = styled(Typography)(({ theme }) => ({
+   width: '75%',
+   textAlign: 'center',
+   padding: '.45rem',
+   borderRadius: '5px',
+   backgroundColor: 'rgba(0,0,0, .65)',
+   margin: '.5rem 0',
+   letterSpacing: '2px',
+   [theme.breakpoints.down('sm')]: {
+      fontSize: '1.4rem',
+   },
+}))
