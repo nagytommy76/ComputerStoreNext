9b05df84b293e89eeddfad686ef95bcf14ae02d7
nagytommy76 <nagytommy76@gmail.com>
2025-04-13 10:48:48 +0200

    Using useMutate hook

--- components/Checkout/Steps/Summary/MakeOrder/MakeOrder.tsx
+++ components/Checkout/Steps/Summary/MakeOrder/MakeOrder.tsx
@@ -1,6 +1,9 @@
+import useMutate from './Hooks/useMutate'
 import Button from '@mui/material/Button'
 
 export default function MakeOrder() {
+   const mutation = useMutate()
+
    return (
       <Button variant='contained' color='primary'>
          Rendelés leadása
