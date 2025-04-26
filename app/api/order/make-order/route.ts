5662c8f319a56a30ab7767d9c50674270b714392
nagytommy76 <nagytommy76@gmail.com>
2025-04-26 11:28:36 +0200

    Get request data
    Get request data & Using switch case to check auth provider

--- app/api/order/make-order/route.ts
+++ app/api/order/make-order/route.ts
@@ -1,10 +1,46 @@
 import { NextResponse, NextRequest } from 'next/server'
+
 import dbConnect from '@DBConnect'
-import { convertSearchParamsToQueryObject } from '@/services/BaseProductRoute'
+import UserModel from '@/models/User/User'
+import GoogleUserModel from '@/models/User/GoogleUser'
 
-export async function GET(request: NextRequest) {
-   const paramsObject = convertSearchParamsToQueryObject(request.nextUrl.searchParams)
-   await dbConnect()
+import type { CartItemsType, ProviderType } from '@/types/userTypes'
 
-   return NextResponse.json({ paramsObject }, { status: 200 })
+export async function POST(request: NextRequest) {
+   const data = (await request.json()) as {
+      cartItems: CartItemsType[]
+      totalPrice: number
+      paymentMethod: string
+      pickUpOption: string
+      authProvider: ProviderType
+      email: string
+   }
+   try {
+      await dbConnect()
+
+      switch (data.authProvider) {
+         case 'google':
+            const googleUser = await GoogleUserModel.findOne({ email: data.email }).select(['orders'])
+            if (!googleUser) throw new Error('User not found')
+            // googleUser.cartItems = data.cartItems
+            // await googleUser.save()
+
+            break
+         default:
+            const user = await UserModel.findOne({ email: data.email }).select(['orders'])
+            if (!user) throw new Error('User not found')
+            // user.orders.push({
+            //    products: data.cartItems,
+            //    totalPrice: data.totalPrice,
+            //    paymentMethod: data.paymentMethod,
+            //    pickUpOption: data.pickUpOption,
+            // })
+
+            break
+      }
+
+      return NextResponse.json({ paramsObject: data }, { status: 200 })
+   } catch (error) {
+      return NextResponse.json({ error }, { status: 500 })
+   }
 }
