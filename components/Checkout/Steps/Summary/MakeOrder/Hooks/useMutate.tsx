09595ebc7087f8873c075f8f87f9eedc64ae9142
nagytommy76 <nagytommy76@gmail.com>
2025-04-27 10:55:16 +0200

    Using router & onSuccess
    Return to main page after checkout & using dispatch to set state (redux, context) to default

--- components/Checkout/Steps/Summary/MakeOrder/Hooks/useMutate.tsx
+++ components/Checkout/Steps/Summary/MakeOrder/Hooks/useMutate.tsx
@@ -1,27 +1,49 @@
+import { useRouter } from 'next/navigation'
 import { useContext } from 'react'
 import { CheckoutContext } from '../../../../Context/CheckoutContext'
 
 import { useMutation } from '@tanstack/react-query'
-import { useAppSelector } from '@/reduxStore/hooks'
+import { useAppSelector, useAppDispatch } from '@/reduxStore/hooks'
+import { setToDefaultState } from '@/reduxStore/slices/Cart/CartSlice'
 
 export default function useMutate() {
+   const dispatch = useAppDispatch()
+   const router = useRouter()
+
    const cartItems = useAppSelector((state) => state.cart.cartItems)
    const totalPrice = useAppSelector((state) => state.cart.totalPrice)
    const {
       authProvider,
       email,
-      checkoutReducer: { paymentMethod, pickUpOption },
+      checkoutReducer: { paymentMethod, pickUpOption, deliveryPrice },
+      checkoutDispatch,
    } = useContext(CheckoutContext)
 
    async function mutationFn() {
       return await fetch(`/api/order/make-order`, {
          method: 'POST',
          headers: { 'Content-Type': 'application/json' },
-         body: JSON.stringify({ cartItems, totalPrice, paymentMethod, pickUpOption, authProvider, email }),
+         body: JSON.stringify({
+            cartItems,
+            totalPrice,
+            paymentMethod,
+            pickUpOption,
+            deliveryPrice,
+            authProvider,
+            email,
+         }),
       })
    }
 
    return useMutation({
       mutationFn,
+      onSuccess(data) {
+         console.log(data.status)
+         if (data.status === 200) {
+            checkoutDispatch({ type: 'SET_TO_DEFAULT', payload: undefined })
+            dispatch(setToDefaultState())
+            router.push('/')
+         }
+      },
    })
 }
