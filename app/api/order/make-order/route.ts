3c77149de5338de5caaef6647529969ef012cbcd
nagytommy76 <nagytommy76@gmail.com>
2025-04-27 10:54:12 +0200

    Add modifyCartItems function
    Push the order details to user & return 200 status code

--- app/api/order/make-order/route.ts
+++ app/api/order/make-order/route.ts
@@ -12,6 +12,7 @@ export async function POST(request: NextRequest) {
       totalPrice: number
       paymentMethod: string
       pickUpOption: string
+      deliveryPrice: number
       authProvider: ProviderType
       email: string
    }
@@ -24,23 +25,49 @@ export async function POST(request: NextRequest) {
             if (!googleUser) throw new Error('User not found')
             // googleUser.cartItems = data.cartItems
             // await googleUser.save()
-
-            break
+            return NextResponse.json({ orderSuccess: true }, { status: 200 })
          default:
             const user = await UserModel.findOne({ email: data.email }).select(['orders'])
             if (!user) throw new Error('User not found')
-            // user.orders.push({
-            //    products: data.cartItems,
-            //    totalPrice: data.totalPrice,
-            //    paymentMethod: data.paymentMethod,
-            //    pickUpOption: data.pickUpOption,
-            // })
+            const modifiedCartItems = modifyCartItems(data.cartItems)
 
-            break
+            user.orders.push({
+               products: modifiedCartItems,
+               totalPrice: data.totalPrice,
+               paymentMethod: data.paymentMethod,
+               deliveryType: data.pickUpOption,
+               deliveryPrice: data.deliveryPrice,
+               orderedAt: new Date(),
+            })
+            // await user.save()
+            return NextResponse.json(
+               { orderSuccess: true, lastOrder: user.orders[user.orders.length - 1] },
+               { status: 200 }
+            )
       }
-
-      return NextResponse.json({ paramsObject: data }, { status: 200 })
    } catch (error) {
       return NextResponse.json({ error }, { status: 500 })
    }
+}
+
+function modifyCartItems(cartItems: CartItemsType[]) {
+   const currentItemsInCart: {
+      productID: string
+      productName: string
+      productQty: number
+      productImage: string
+      productPrice: number
+      productType: string
+   }[] = cartItems.map((product) => {
+      return {
+         productID: product.itemId,
+         productImage: product.displayImage,
+         productName: product.displayName,
+         productQty: product.quantity,
+         productPrice: product.price,
+         productType: product.productType,
+      }
+   })
+
+   return currentItemsInCart
 }
